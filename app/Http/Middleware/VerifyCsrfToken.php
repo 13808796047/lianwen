882d28faf416d934582fe45908ab3e9cbaba5b9b
Summer <511391805@qq.com>
2020-03-15 11:12:41 +0800

    微信支付配置:bookmark:

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,6 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         'payments/alipay/notify',
-        'payment/wechat/notify',
+        'payments/wechat/notify',
     ];
 }
