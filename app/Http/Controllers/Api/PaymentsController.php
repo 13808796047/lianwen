31dca78f09bd057be05800be929a82ec8e544bcc
Summer <csy13808796047@gmail.com>
2020-03-16 17:35:15 +0800

    支付宝扫码支付:bookmark:

--- app/Http/Controllers/Api/PaymentsController.php
+++ app/Http/Controllers/Api/PaymentsController.php
@@ -19,13 +19,10 @@ class PaymentsController extends Controller
 //            throw new InvalidRequestException('订单状态不正确!');
 //        }
         // 调用支付宝的网页支付
-        $data = app('alipay')->web([
+        return app('alipay')->web([
             'out_trade_no' => time(), // 订单编号，需保证在商户端不重复
             'total_amount' => 0.01, // 订单金额，单位元，支持小数点后两位
             'subject' => '支付 联文检测 的订单：' . time(), // 订单标题
-        ]);
-        return response()->json([
-            'data' => $data
         ]);
     }
 
