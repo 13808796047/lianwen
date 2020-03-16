803bd6c4f9f6387aab9af24eefab08b57b2d55e6
Summer <csy13808796047@gmail.com>
2020-03-16 10:41:57 +0800

    修改注册登录字段为手机号:bookmark:

--- app/Http/Requests/Api/UserRequest.php
+++ app/Http/Requests/Api/UserRequest.php
@@ -9,7 +9,7 @@ class UserRequest extends FormRequest
     public function rules()
     {
         return [
-            'phone' => 'required|regex:/^((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199)\d{8}$/|unique:users,phone',
+            'phone' => 'required|regex:^((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199)\d{8}$|unique:users,phone',
             'password' => 'required|alpha_dash|min:6',
             'verification_key' => 'string',
             'verification_code' => 'string',
