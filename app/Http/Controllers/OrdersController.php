716abc9a5679164bcf312ab9165f581099a875a2
Summer <csy13808796047@gmail.com>
2020-03-17 09:42:02 +0800

    测试队列:bookmark:

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Handlers\OrderApiHandler;
+use App\Jobs\CheckOrderStatus;
 use App\Models\Order;
 use Illuminate\Http\Request;
 
@@ -15,6 +16,8 @@ class OrdersController extends Controller
 
     public function show(Order $order, Request $request, OrderApiHandler $apiHandler)
     {
+        $this->dispatch(new CheckOrderStatus($order));
+        
         return view('orders.show', ['order' => $order->load('category')]);
     }
 }
