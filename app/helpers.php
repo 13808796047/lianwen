d3455dab3c451488ab42793faa61e5524f7e13a4
Summer <csy13808796047@gmail.com>
2020-03-17 14:40:38 +0800

    测试队列:bookmark:

--- app/helpers.php
+++ app/helpers.php
@@ -13,38 +13,38 @@ function remove_spec_char($content)
     return $txt;
 }
 
-/**
- * 纯php读取docx文档
- * @param unknown $filename
- */
-function read_docx($filename)
-{
-
-    $striped_content = '';
-    $content = '';
-
-    if(!$filename || !file_exists($filename)) return false;
-
-    $zip = zip_open($filename);
-    if(!$zip || is_numeric($zip)) return false;
-
-    while ($zip_entry = zip_read($zip)) {
-
-        if(zip_entry_open($zip, $zip_entry) == false) continue;
-
-        if(zip_entry_name($zip_entry) != "word/document.xml") continue;
-
-        $content .= zip_entry_read($zip_entry, zip_entry_filesize($zip_entry));
-
-        zip_entry_close($zip_entry);
-    }
-    zip_close($zip);
-    $content = str_replace('</w:r></w:p></w:tc><w:tc>', " ", $content);
-    $content = str_replace('</w:r></w:p>', "\r\n", $content);
-    $striped_content = strip_tags($content);
-
-    return remove_spec_char($striped_content);
-}
+///**
+// * 纯php读取docx文档
+// * @param unknown $filename
+// */
+//function read_docx($filename)
+//{
+//
+//    $striped_content = '';
+//    $content = '';
+//
+//    if(!$filename || !file_exists($filename)) return false;
+//
+//    $zip = zip_open($filename);
+//    if(!$zip || is_numeric($zip)) return false;
+//
+//    while ($zip_entry = zip_read($zip)) {
+//
+//        if(zip_entry_open($zip, $zip_entry) == false) continue;
+//
+//        if(zip_entry_name($zip_entry) != "word/document.xml") continue;
+//
+//        $content .= zip_entry_read($zip_entry, zip_entry_filesize($zip_entry));
+//
+//        zip_entry_close($zip_entry);
+//    }
+//    zip_close($zip);
+//    $content = str_replace('</w:r></w:p></w:tc><w:tc>', " ", $content);
+//    $content = str_replace('</w:r></w:p>', "\r\n", $content);
+//    $striped_content = strip_tags($content);
+//
+//    return remove_spec_char($striped_content);
+//}
 
 /**
  * 依赖于windows word com 组件
