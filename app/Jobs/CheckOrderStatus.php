7f58775358fffc96f2f552161d04c6a4bab81510
Summer <csy13808796047@gmail.com>
2020-03-18 09:46:40 +0800

    测试队列:bookmark:

--- app/Jobs/CheckOrderStatus.php
+++ app/Jobs/CheckOrderStatus.php
@@ -31,7 +31,7 @@ class CheckOrderStatus implements ShouldQueue
         $api = new OrderApiHandler();
         $order = $api->getOrder($this->order->api_orderid);
         //判断对应的订单是否已经被支付
-        if($this->status == 5) {
+        if($this->order->status == 5) {
             $file = $api->downloadReport($this->order->api_orderid);
             $path = 'downloads\report-' . $this->order->api_orderid . '.zip';
             $result = \Storage::disk('local')->put($path, $file);
@@ -52,7 +52,7 @@ class CheckOrderStatus implements ShouldQueue
         } else {
             $status = 4;
         }
-        \DB::transaction(function() {
+        \DB::transaction(function() use ($order, $status) {
             $this->order->update([
                 'rate' => $order->data->orderCheck->apiResultSemblance,
                 'status' => $status,
