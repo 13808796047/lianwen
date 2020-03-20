3f7ef8b6258ffca822bc9798d181559218ef207a
Summer <csy13808796047@gmail.com>
2020-03-20 17:29:11 +0800

    微信社会化登录:bookmark:

--- app/Http/Controllers/AuthenticationsController.php
+++ app/Http/Controllers/AuthenticationsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Overtrue\Socialite\SocialiteManager;
 
@@ -32,8 +33,24 @@ class AuthenticationsController extends Controller
         $oauthUser = $this->app->driver($type)->user();
         switch ($type) {
             case 'wechat':
-                $unionid = $oauthUser->offsetExists('unionid') ? $oauthUser->offsetGet('unionid') : null;
-                dd($unionid);
+                $unionid = $oauthUser->getOriginal()['unionid'] ?: null;
+                if($unionid) {
+                    $user = User::where('weixin_unionid', $unionid)->first();
+                } else {
+                    $user = User::where('weixin_openid', $oauthUser->getOriginal()['openid'])->first();
+                }
+                // 没有用户，默认创建一个用户
+                if(!$user) {
+                    $user = User::create([
+                        'name' => $oauthUser->getNickname(),
+                        'avatar' => $oauthUser->getAvatar(),
+                        'weixin_openid' => $oauthUser->getId(),
+                        'weixin_unionid' => $oauthUser->getOriginal()['openid'],
+                    ]);
+                }
+                break;
         }
+        auth('web')->login($user);
+        return redirect()->to('pages.index');
     }
 }
