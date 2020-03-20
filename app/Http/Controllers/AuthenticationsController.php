4131400fa8e71206f7109c20ea3afb8492df4865
Summer <csy13808796047@gmail.com>
2020-03-20 16:15:56 +0800

    账号/手机号登录:bookmark:

--- app/Http/Controllers/AuthenticationsController.php
+++ app/Http/Controllers/AuthenticationsController.php
@@ -7,14 +7,24 @@ use Illuminate\Http\Request;
 
 class AuthenticationsController extends Controller
 {
+    protected $app;
+
     public function __construct()
     {
-        $this->app = Factory::officialAccount(config('wechat.official_account'));
+        $config = [
+            'app_id' => 'wxdaab22b871fc3982', // AppID
+            'secret' => '6680c8ec8bd33997d3f709b889f36d17', // AppSecret
+            'oauth' => [
+                'scopes' => ['snsapi_userinfo'],
+                'callback' => '/oauth/weixin/callback',
+            ],
+        ];
+        $this->app = Factory::officialAccount($config);
     }
 
     public function oauth($type, Request $request)
     {
-        return \Socialite::with($type)->redirect();
+        return $this->app->oauth->redirect();
     }
 
     public function callback($type, Request $request)
