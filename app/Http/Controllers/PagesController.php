c59eb7bca4673387bf16b71da3e1eccaa7e22692
Summer <csy13808796047@gmail.com>
2020-03-20 12:45:37 +0800

    账号/手机号登录:bookmark:

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -4,10 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+
 class PagesController extends Controller
 {
+    protected $app;
+
+    public function __construct()
+    {
+        $this->app = app('wechat.official_account');
+    }
+
     public function index()
     {
-        return view('pages.index');
+        $result = $this->app->qrcode->temporary('foo', 600);
+        $qrcodeUrl = $this->app->qrcode->url($result['ticket']);
+        return view('pages.index', compact('qrcodeUrl'));
     }
 }
