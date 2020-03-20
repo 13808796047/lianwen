140d25b925f61b9987117ef5054db99b5f914819
Summer <csy13808796047@gmail.com>
2020-03-20 16:05:08 +0800

    账号/手机号登录:bookmark:

--- app/Http/Controllers/AuthenticationsController.php
+++ app/Http/Controllers/AuthenticationsController.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use EasyWeChat\Factory;
 use Illuminate\Http\Request;
 
 class AuthenticationsController extends Controller
 {
+    public function __construct()
+    {
+        $this->app = Factory::officialAccount(config('wechat.official_account'));
+    }
+
     public function oauth($type, Request $request)
     {
         return \Socialite::with($type)->redirect();
