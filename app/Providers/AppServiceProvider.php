2f2867b345aa7eb232ab1a41a476225632ff1c07
Summer <csy13808796047@gmail.com>
2020-03-24 15:35:04 +0800

    退出登录:bookmark:

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -36,7 +36,7 @@ class AppServiceProvider extends ServiceProvider
         $this->app->singleton('alipay_wap', function() {
             $config = config('pay.alipay');
             $config['notify_url'] = route('payments.alipay.notify');
-            $config['return_url'] = '';
+            $config['return_url'] = 'http://h5.lianwen.com/#/payment';
             //判断当前项目运行环境是否为线上环境
             if(app()->environment() != 'production') {
                 $config['mode'] = 'dev';
