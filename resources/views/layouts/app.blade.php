017752c121f0d279c3f52cb3101f17f70008368d
Summer <csy13808796047@gmail.com>
2020-03-24 16:20:23 +0800

    退出登录:bookmark:

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,29 +32,47 @@
 
 <!-- Scripts -->
 <script src="{{ mix('js/app.js') }}"></script>
-@yield('scripts')
-<script !src="">
-  //退出登录
-  $('.logout').click(() => {
-    swal({
-      title: "您确认要退出登录吗?",
-      icon: "warning",
-      buttons: true,
-      dangerMode: true,
-    })
-      .then((willDelete) => {
-        if (willDelete) {
-          axios.post('{{route('logout')}}').then(res => {
-            swal("注销成功!", {
-              icon: "success",
-            }).then(willDelete => {
-              location.reload();
-            });
-          })
-        }
-      });
-  });
+<script>
+  //H5移动版自适应跳转js
+  var mobileAgent = new Array("iphone", "ipod", "ipad", "android", "mini", "mobile", "mobi", "mqqbrowser", "blackberry",
+    "webos", "incognito", "webmate", "bada", "nokia", "symbian", "wp7", "wp8", "lg", "ucweb", "skyfire");
+  var browser = navigator.userAgent.toLowerCase();
+  var _tag = "{$_GET['tag']}";
+  if (_tag != 'web') {
+    for (var i = 0; i < mobileAgent.length; i++) {
+      if (browser.indexOf(mobileAgent[i]) != -1) {
+        window.location.href = 'http://www.xxx.com';
+        break;
+      }
+    }
+  }
 </script>
-</body>
+@yield('scripts')
+<
+script
+!src = "" >
+//退出登录
+$('.logout').click(() => {
+swal({
+title: "您确认要退出登录吗?",
+icon: "warning",
+buttons: true,
+dangerMode: true,
+})
+.then((willDelete) => {
+if (willDelete) {
+axios.post('{{route('logout')}}').then(res => {
+swal("注销成功!", {
+icon: "success",
+}).then(willDelete => {
+location.reload();
+});
+})
+}
+});
+});
+</script>
+<
+/body>
 
-</html>
+< /html>
