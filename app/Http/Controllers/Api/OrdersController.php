d8eceabbd89373d6069ea5b78f28de2a8eb7d198
Summer <csy13808796047@gmail.com>
2020-03-25 14:35:55 +0800

    订单列表:bookmark:

--- app/Http/Controllers/Api/OrdersController.php
+++ app/Http/Controllers/Api/OrdersController.php
@@ -50,7 +50,7 @@ class OrdersController extends Controller
 
     public function index(Request $request)
     {
-        $orders = $request->user()->orders()->with('category:cid,name')->orderBy('created_at')->get();;
+        $orders = $request->user()->orders()->with('category:cid,name')->orderBy('created_at', 'desc')->get();;
         return OrderResource::collection($orders);
     }
 
