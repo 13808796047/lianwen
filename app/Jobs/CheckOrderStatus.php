c98069dab4c24b91d8d40fe9edfb2b3163b46a9e
Summer <511391805@qq.com>
2020-03-26 21:12:57 +0800

    队列任务:seedling:

--- app/Jobs/CheckOrderStatus.php
+++ app/Jobs/CheckOrderStatus.php
@@ -16,25 +16,25 @@ class CheckOrderStatus implements ShouldQueue
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     protected $order;
-    protected $api;
 
     public function __construct(Order $order)
     {
         $this->order = $order;
-        $this->api = app(OrderApiHandler::class);
+
     }
 
     //定义这个任务类具体的执行逻辑
     //当队列处理器从队列中取出任务时，会调用handle()方法
     public function handle()
     {
-        $apiOrder = $this->api->getOrder($this->order->api_orderid);
+        $api = app(OrderApiHandler::class);
+        $apiOrder = $api->getOrder($this->order->api_orderid);
         //判断对应的订单是否已经被支付
         if($this->order->status == OrderEnum::CHECKED) {
-            $file = $this->api->downloadReport($this->order->api_orderid);
+            $file = $api->downloadReport($this->order->api_orderid);
             $path = 'downloads/report-' . $this->order->api_orderid . '.zip';
             $result = \Storage::put($path, $file);
-            $report = $this->api->extractReportDetail($this->order->api_orderid);
+            $report = $api->extractReportDetail($this->order->api_orderid);
             if($report) {
                 \DB::transaction(function() use ($path, $report) {
                     $this->order->update([
