86ecbda84db058c0be7e6ae8cbfb963c59bf6f2a
Summer <511391805@qq.com>
2020-03-27 21:32:42 +0800

    队列任务:seedling:

--- app/Admin/Controllers/OrdersController.php
+++ app/Admin/Controllers/OrdersController.php
@@ -32,24 +32,33 @@ class OrdersController extends AdminController
     protected function grid()
     {
         $grid = new Grid(new Order());
-
-        $grid->column('orderid', '订单号');
+        $grid->fixColumns(3);
+        $grid->quickSearch('orderid');
+        $grid->column('orderid', '订单号')->totalRow('合计');
         $grid->column('category.name', '分类');
         // 展示关联关系的字段时，使用 column 方法
-        $grid->column('user.name', '买家');
-        $grid->column('price', '金额');
+        $grid->column('user.phone', '买家');
+        $grid->column('price', '金额')->totalRow(function($amount) {
+
+            return "<span class='text-danger text-bold'><i class='fa fa-yen'></i> {$amount} 元</span>";
+
+        });
         $grid->column('title', '标题');
         $grid->column('writer', '作者');
         $grid->column('date_publish', '发表时间');
         $grid->column('words', '字数');
-        $grid->column('pay_price', '支付金额');
+        $grid->column('pay_price', '支付金额')->totalRow(function($amount) {
+
+            return "<span class='text-danger text-bold'><i class='fa fa-yen'></i> {$amount} 元</span>";
+
+        });
         $grid->column('pay_type', '支付方式');
         $grid->column('payid', '支付ID');
-        $grid->column('date_pay', '支付日期');
+        $grid->column('date_pay', '支付日期')->sortable();
         $grid->column('rate', '完成率');
         $grid->column('result', '结果');
         $grid->column('from', '来源');
-        $grid->column('created_at', '创建时间');
+        $grid->column('created_at', '创建时间')->sortable();
         $grid->column('api_orderid', 'api订单ID');
         // 禁用创建按钮，后台不需要创建订单
         $grid->disableCreateButton();
