91a2bea694e3f64a3683413a31967ab54cfc6e16
Summer <csy13808796047@gmail.com>
2020-03-28 13:17:37 +0800

    页头和页脚:bookmark:

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -55,7 +55,7 @@
                 </div>
                 <div class="mb-6">
                   <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
-                    密码
+                    密码:
                   </label>
                   <input
                     class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
@@ -69,8 +69,9 @@
                     type="button" id="btnSubmit">
                     登录
                   </button>
-                  <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
-                    忘记密码?
+                  <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+                     href="{{route('register')}}">
+                    还没有账号?去注册
                   </a>
                 </div>
                 <p class=" text-gray-500 text-xs mt-4">
