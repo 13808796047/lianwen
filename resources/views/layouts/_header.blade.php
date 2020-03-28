10650969deb8eafd77e44a6bc811282537b05f48
Summer <csy13808796047@gmail.com>
2020-03-28 13:25:28 +0800

    页头和页脚:bookmark:

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,6 +1,6 @@
 <nav class="flex items-center justify-between flex-wrap bg-white-100">
   <div class="flex items-center flex-shrink-0 text-white mr-6 bg-blue-600 p-2">
-    <img src="https://css.lianwen.com/logo/2019/weipudx.png" alt="">
+    <a href="/"><img src="https://css.lianwen.com/logo/2019/weipudx.png" alt=""></a>
   </div>
   <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
     <div class="text-sm lg:flex-grow">
