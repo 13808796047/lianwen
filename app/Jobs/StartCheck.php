549aaa18b6f0d15243b24020b8af2fec6fbd6fd8
Summer <511391805@qq.com>
2020-04-05 13:11:09 +0800

    开始检测条件:seedling:

--- app/Jobs/StartCheck.php
+++ app/Jobs/StartCheck.php
@@ -27,8 +27,11 @@ class StartCheck implements ShouldQueue
         $api = app(OrderApiHandler::class);
         $result = $api->startCheck($this->order->api_orderid);
         info('开始检测....');
-        if($result->code == 200) {
+        if($result->code == 200 && $this->order->status == 1) {
             dispatch(new getOrderStatus($this->order))->delay(now()->addMinutes());
+            $this->order->update([
+                'status' => 3,
+            ]);
         }
     }
 }
