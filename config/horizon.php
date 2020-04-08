9d75d2620468936bea794df8f13eb2c4d527cfa3
Summer <511391805@qq.com>
2020-04-08 20:20:49 +0800

    队列超时:bookmark:

--- config/horizon.php
+++ config/horizon.php
@@ -56,7 +56,7 @@ return [
 
     'prefix' => env(
         'HORIZON_PREFIX',
-        Str::slug(env('APP_NAME', 'laravel'), '_').'_horizon:'
+        Str::slug(env('APP_NAME', 'laravel'), '_') . '_horizon:'
     ),
 
     /*
@@ -171,7 +171,7 @@ return [
                 'queue' => ['default'],
                 'balance' => 'simple',
                 'processes' => 10,
-                'tries' => 1,
+                'tries' => 0,
             ],
         ],
 
