53fe8cb0163db80c6b7480b0c04815307763241e
Summer <csy13808796047@gmail.com>
2020-04-11 17:14:57 +0800

    首页:seedling:

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('v1')
     ->namespace('Api')
     ->name('api.v1')
-//    ->middleware('throttle:' . config('api.rate_limits.sign'))
+    ->middleware('throttle:' . config('api.rate_limits.sign'))
     ->group(function() {
         Route::middleware('auth:api')->group(function() {
 
