51002261c0db61e508885758a9bb15b0de1b28d3
Summer <csy13808796047@gmail.com>
2020-04-15 13:30:08 +0800

    限制doc:seedling:

--- resources/views/www_zcnki_com/layouts/app.blade.php
+++ resources/views/www_zcnki_com/layouts/app.blade.php
@@ -23,7 +23,7 @@
     if (_tag != 'web') {
       for (var i = 0; i < mobileAgent.length; i++) {
         if (browser.indexOf(mobileAgent[i]) != -1) {
-          window.location.href = 'https://wap.lianwen.com';
+          window.location.href = 'https://wap.lianwen.com/wanfang';
           break;
         }
       }
