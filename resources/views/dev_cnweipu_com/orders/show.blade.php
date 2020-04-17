2ee5df8d672e58e9f9425bd76fa759da0be12aed
阿沙冲冲冲 <361719196@qq.com>
2020-04-17 14:19:45 +0800

    支付二维码修改

--- resources/views/dev_cnweipu_com/orders/show.blade.php
+++ resources/views/dev_cnweipu_com/orders/show.blade.php
@@ -159,7 +159,7 @@
       $('#btn-wechat').click(function () {
         swal({
           // content 参数可以是一个 DOM 元素，这里我们用 jQuery 动态生成一个 img 标签，并通过 [0] 的方式获取到 DOM 元素
-          content: $('<img src="{{ route('payments.wechat', ['order' => $order->id]) }}" />')[0],
+          content: $('<img src="{{ route('payments.wechat', ['order' => $order->id]) }}" style="display: block;margin: 0 auto;"/>')[0],
           // buttons 参数可以设置按钮显示的文案
           buttons: ['关闭', '已完成付款'],
         })
