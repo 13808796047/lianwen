93b1027c6810b387f77d04a34d76f844929d1c60
阿沙冲冲冲 <361719196@qq.com>
2020-04-17 16:20:55 +0800

    注册页面

--- resources/views/dev_cnweipu_com/auth/register.blade.php
+++ resources/views/dev_cnweipu_com/auth/register.blade.php
@@ -16,6 +16,9 @@
 
 <div class="main clearfix" style="background:#fff">
 <div style="padding:25px 500px;">
+            <div>
+                <div role="alert" id="message" style="display: none">
+                </div>
     <form action="{{route('register')}}" method="post" accept-charset="utf-8">
                 @csrf
                 <!--	<div style="padding:10px 0">
