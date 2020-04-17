dd24f0ff1ec7116d0754f66dc97548d55a7eacf3
阿沙冲冲冲 <361719196@qq.com>
2020-04-17 13:49:02 +0800

    支付页修改

--- resources/views/dev_cnweipu_com/orders/show.blade.php
+++ resources/views/dev_cnweipu_com/orders/show.blade.php
@@ -19,32 +19,19 @@
 {{--      </h5>--}}
 {{--    </div>--}}
 {{--  </div>--}}
-<div class="container-fluid">
-  <!-- Main window -->
-  <div
-    id="dashboard_page"
-    style="padding:15px 0;"
-  >
-    <div class="row-fluid">
-      <div class="widget widget-padding span12">
-        <div class="widget-header">
-          <i class="icon-bar-chart"></i>
-          <h5>支付</h5>
-          <div class="widget-buttons">
-            <a><i data-title="Collapse" class="icon-chevron-up"></i></a>
-          </div>
-        </div>
-        <div class="widget-body">
+<div class="main clearfix">
+  <div class="lbox fl">
+  <div class="widget-body">
           <div class="cbox submit yh">
             <div class="down clearfix">
               <form action="{{route('payments.alipay',['order'=>$order->orderid])}}" method="get">
                 <table class="mylist" style="line-height: 30px">
                   <tr>
                     <td width="149">
-                      <span>检测编号</span>
+                      <span class="hl bred">检测编号(请牢记)</span>
                     </td>
                     <td style="text-align: left;">
-                      <span>{{ $order->orderid }}</span>
+                      <span class="hl bgreen">{{ $order->orderid }}</span>
                     </td>
                   </tr>
                   <tr>
@@ -159,24 +146,20 @@
                       在线支付<font color="#FF0004">(推荐)</font>
                     </td>
                     <td class="td">
-                    <div style="display:flex;align-items: center;">
                       <input
                         type="radio"
                         name="paytype"
                         value="alipay"
                         checked="checked"
                       />
-                      <img src="{{asset('asset/images/alipay.png')}}" style="margin-left:17px;"/>
-                      </div>
+                      <img src="{{asset('asset/images/alipay.png')}}"/>
                       &nbsp;&nbsp;
-                      <div style="display:flex;align-items: center;">
                       <!--<label><input type="radio" name="paytype" value="tenpay" > <img src="./asset/imgs/tenpay.png" /></label> -->
                       <input type="radio" name="paytype" value="wxpay"/>
                       <img
                         src="{{asset('asset/images/wxpay.png')}}"
-                        style="width:99px;margin-left:17px"
+                        style="width:99px"
                       />
-                      </div>
                       <div class="tips">
                         直接使用支付宝或者微信支付，即时返回支付结果，方便快捷，推荐使用。
                       </div>
@@ -196,7 +179,13 @@
                    href="javascript:;" class="btn btn-primary btn-sm sbtn">提交</a>
               </form>
               <div class="clearfix"></div>
-              <div class="tipsbox">
+             
+            </div>
+          </div>
+        </div>
+  </div>     
+  <div class="rbox fr">
+  <div class="tipsbox">
                 <span class="hl bgreen">温馨提示</span>
                 <p>
                   1、如果您的可用余额、代金券/秘钥面值与实际值不符，因为您有未完成的提现申请或未完成的检测订单，造成了相应金额的锁定。
@@ -205,18 +194,7 @@
                   2、如果您勾选了余额支付和代金券/秘钥支付，将优先使用代金券/秘钥支付，余下部分才从账户余额支付。
                 </p>
               </div>
-            </div>
-          </div>
-        </div>
-        <!-- /widget body -->
-      </div>
-      <!-- /widget span12 -->
-    </div>
-    <!-- /row-fluid -->
-    <!--修改-->
-    <!--            --><!--修改-->
   </div>
-  <!-- /Main window -->
 </div>
 <!--/.fluid-container-->
 
