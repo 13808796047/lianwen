18c522a71e2fc35be560707b9cdd0f32c1799af9
阿沙冲冲冲 <361719196@qq.com>
2020-04-18 11:20:44 +0800

    登录样式3

--- resources/views/dev_cnweipu_com/pages/index.blade.php
+++ resources/views/dev_cnweipu_com/pages/index.blade.php
@@ -142,7 +142,7 @@
           <div class="modal-footer" style="justify-content: space-between;">
             <div style="display: flex;align-items: center;"> 
                <p class="text-sm">社交账号登录</p>
-               <a href="{{route('oauth',['type'=>'wechat'])}}" class="block mr-4" style="padding-left:23px">
+               <a data-href="{{route('oauth',['type'=>'wechat'])}}" id="toWechat" class="block mr-4" style="padding-left:23px">
                   <svg t="1585367706568" class="icon" viewBox="0 0 1024 1024" version="1.1"
                      xmlns="http://www.w3.org/2000/svg" p-id="1112" width="24" height="24">
                    <path
@@ -605,7 +605,9 @@
       //   }
       // });
       // $().UItoTop({easingType: 'easeOutQuart'});
-
+      $('#toWechat').click(function (e) {
+        console.log(e,3123123)
+      })
       // Tab切换
       $('.banner-li').click(function () {
         $(this)
