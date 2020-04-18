2207954f5b56cf0310bfadb5a630b4fda9c791aa
阿沙冲冲冲 <361719196@qq.com>
2020-04-18 12:23:45 +0800

    bgtips

--- resources/views/dev_cnweipu_com/orders/show.blade.php
+++ resources/views/dev_cnweipu_com/orders/show.blade.php
@@ -136,7 +136,7 @@
 		</div>
 	</div>
 	<div class="rbox fr">
-		<div style="background:#fff;">
+		<div style="background:#fff;padding:20px;">
 		    <b>1、检测结果是否准确？</b>
         <p>如果你们学校也是用万方检测，那结果是一致的。同一个的系统、同样的比对库、同样的算法，所以只要在本系统提交的内容和学校的一致，那检测结果是一致的。</p>
         <b>2、检测需要多少时间？</b>
