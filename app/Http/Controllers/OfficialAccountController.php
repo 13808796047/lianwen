2fbb6ce04252abbf448789204b19e68258335dc1
Summer <csy13808796047@gmail.com>
2020-04-20 17:22:36 +0800

    微信公众号:seedling:

--- app/Http/Controllers/OfficialAccountController.php
+++ app/Http/Controllers/OfficialAccountController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use EasyWeChat\Factory;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class OfficialAccountController extends Controller
 {
