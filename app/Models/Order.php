150455602fdafd9315cf912a2df3b4d9c0147195
Summer <csy13808796047@gmail.com>
2020-04-21 11:59:08 +0800

    微信公众号:seedling:

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -14,6 +14,10 @@ class Order extends Model
     use DefaultDatetimeFormat;
     use SoftDeletes;
     protected $guarded = [];
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d H:i:s',
+        'updated_at' => 'datetime:Y-m-d H:i:s'
+    ];
 
     public function user()
     {
