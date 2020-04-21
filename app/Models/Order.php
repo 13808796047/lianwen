a8e0384c18f458546feacffbabb3c353da6fb592
Summer <csy13808796047@gmail.com>
2020-04-21 12:04:25 +0800

    微信公众号:seedling:

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -19,6 +19,11 @@ class Order extends Model
         'updated_at' => 'datetime:Y-m-d H:i:s'
     ];
 
+    protected function serializeDate(\DateTimeInterface $date)
+    {
+        return $date->format('Y-m-d H:i:s');
+    }
+
     public function user()
     {
         return $this->belongsTo(User::class, 'userid');
