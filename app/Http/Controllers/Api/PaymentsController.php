4ea10c6f8d2e7790bca964fdbe633e3371a7459b
Summer <csy13808796047@gmail.com>
2020-04-22 14:38:54 +0800

    微信公众号:seedling:

--- app/Http/Controllers/Api/PaymentsController.php
+++ app/Http/Controllers/Api/PaymentsController.php
@@ -25,8 +25,7 @@ class PaymentsController extends Controller
             throw new InvalidRequestException('订单状态不正确');
         }
         $openid = $request->user()->weapp_openid;
-        dd($openid);
-        return app('wechat_pay_wap')->mp([
+        return app('wechat_pay_mp')->mp([
             'out_trade_no' => $order->orderid,  // 商户订单流水号，与支付宝 out_trade_no 一样
             'total_fee' => $order->price * 100, // 与支付宝不同，微信支付的金额单位是分。
             'body' => '支付' . $order->category->name . ' 的订单：' . $order->orderid, // 订单描述
