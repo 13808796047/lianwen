2c873a67e85f9b6e8fe9c73202e755414b7e3b00
Summer <csy13808796047@gmail.com>
2020-04-23 11:23:03 +0800

    加入创建订单参数:seedling:

--- app/Handlers/OrderApiHandler.php
+++ app/Handlers/OrderApiHandler.php
@@ -159,6 +159,7 @@ class OrderApiHandler
                 'Token' => $this->token
             ],
         ];
+        dd($this->api . 'order/extract-report-pdf/A237072768');
         $response = $this->http->get($this->api . 'order/extract-report-pdf/A237072768', $option);
         dd($response);
         dd(json_decode($response->getbody()->getContents()));
