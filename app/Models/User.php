23340f74793520e4029289e801242ca86d0c9e1a
Summer <csy13808796047@gmail.com>
2020-04-23 17:11:36 +0800

    绑定手机:seedling:

--- app/Models/User.php
+++ app/Models/User.php
@@ -64,7 +64,7 @@ class User extends Authenticatable implements JWTSubject
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token', 'weixin_openid', 'weixin_unionid', 'weapp_openid', 'weixin_session_key'
+        'password', 'remember_token',
     ];
 
     /**
