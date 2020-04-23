e7037e6a88e053b75dc11255616ba4a3b819ef8d
Summer <csy13808796047@gmail.com>
2020-04-23 16:56:52 +0800

    绑定手机:seedling:

--- app/Http/Requests/Api/BoundPhoneRequest.php
+++ app/Http/Requests/Api/BoundPhoneRequest.php
@@ -9,9 +9,16 @@ class BoundPhoneRequest extends FormRequest
     public function rules()
     {
         return [
-            'required',
-            'regex:/^((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199)\d{8}$/',
-            'unique:users'
+            'verification_key' => 'required|string',
+            'verification_code' => 'required|string',
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'verification_key' => '短信验证码 key',
+            'verification_code' => '短信验证码',
         ];
     }
 }
