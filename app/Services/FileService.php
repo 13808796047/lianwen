159771b884ee8d022b948979abded4440da81cb7
Summer <csy13808796047@gmail.com>
2020-04-24 17:54:19 +0800

    文件异步上传:seedling:

--- app/Services/FileService.php
+++ app/Services/FileService.php
@@ -10,12 +10,13 @@ class FileService
 {
     public function add($request, $uploadHandler)
     {
+        $user = auth()->user();
         if($file = $request->file) {
             $type = $file->getClientOriginalExtension() == 'docx' ? 'docx' : 'txt';
-            $result = $uploadHandler->save($file, 'files', $request->user()->id);
+            $result = $uploadHandler->save($file, 'files', $user->id);
             $data = File::create([
                 'type' => $type,
-                'user_id' => $request->user()->id,
+                'user_id' => $user->id,
                 'path' => $request['path']
             ]);
         }
