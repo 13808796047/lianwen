36d97e9dcf1ce0059a65a2bcd6e4c5e346125ce3
Summer <csy13808796047@gmail.com>
2020-04-24 11:12:46 +0800

    绑定手机:seedling:

--- app/Admin/Controllers/OrdersController.php
+++ app/Admin/Controllers/OrdersController.php
@@ -34,58 +34,59 @@ class OrdersController extends AdminController
      */
     protected function grid()
     {
+
         $grid = new Grid(new Order());
-        $grid->paginate(10);
-        $grid->model()->orderBy('created_at', 'desc');
-        $grid->filter(function($filter) {
-            // 去掉默认的id过滤器
-            $filter->disableIdFilter();
-            // 在这里添加字段过滤器
-            $filter->like('title', '标题');
-            $filter->like('writer', '作者');
-            $filter->like('orderid', '订单号');
-            $filter->like('api_orderid', 'api订单ID');
-            $filter->like('from', '来源');
-            $filter->like('user.phone', '手机号');
-            $filter->like('category.name', '检测系统');
-            $filter->scope('1', '已支付')->where('status', 1);
-            $filter->scope('3', '检测中')->where('status', 3);
-            $filter->scope('4', '检测完成')->where('status', 4);
-            $filter->scope('0', '未支付')->where('status', 0);
-        });
-        $grid->fixColumns(3);
-        $grid->column('orderid', '订单号')->totalRow('合计');
-        $grid->column('category.name', '分类');
-        // 展示关联关系的字段时，使用 column 方法
-        $grid->column('userid', '买家')->display(function($userid) {
-            return User::find($userid)->phone ?? User::find($userid)->nick_name;
-        });
-        $grid->column('status', '状态')->using([
-            0 => '未支付',
-            1 => '待检测',
-            2 => '排队中',
-            3 => '检测中',
-            4 => '检测完成'
-        ])->dot([
-            0 => 'danger',
-            1 => 'info',
-            2 => 'primary',
-            3 => 'warning',
-            4 => 'success',
-        ]);
-        $grid->column('title', '标题');
-        $grid->column('writer', '作者');
-        $grid->column('words', '字数');
-        $grid->column('pay_price', '支付金额')->totalRow(function($amount) {
-
-            return "<span class='text-danger text-bold'><i class='fa fa-yen'></i> {$amount} 元</span>";
-
-        });
-        $grid->column('pay_type', '支付方式');
-        $grid->column('from', '来源');
-        $grid->column('created_at', '创建时间')->sortable();
-        // 禁用创建按钮，后台不需要创建订单
-        $grid->disableCreateButton();
+//        $grid->paginate(10);
+//        $grid->model()->orderBy('created_at', 'desc');
+//        $grid->filter(function($filter) {
+//            // 去掉默认的id过滤器
+//            $filter->disableIdFilter();
+//            // 在这里添加字段过滤器
+//            $filter->like('title', '标题');
+//            $filter->like('writer', '作者');
+//            $filter->like('orderid', '订单号');
+//            $filter->like('api_orderid', 'api订单ID');
+//            $filter->like('from', '来源');
+//            $filter->like('user.phone', '手机号');
+//            $filter->like('category.name', '检测系统');
+//            $filter->scope('1', '已支付')->where('status', 1);
+//            $filter->scope('3', '检测中')->where('status', 3);
+//            $filter->scope('4', '检测完成')->where('status', 4);
+//            $filter->scope('0', '未支付')->where('status', 0);
+//        });
+//        $grid->fixColumns(3);
+//        $grid->column('orderid', '订单号')->totalRow('合计');
+//        $grid->column('category.name', '分类');
+//        // 展示关联关系的字段时，使用 column 方法
+//        $grid->column('userid', '买家')->display(function($userid) {
+//            return User::find($userid)->phone ?? User::find($userid)->nick_name;
+//        });
+//        $grid->column('status', '状态')->using([
+//            0 => '未支付',
+//            1 => '待检测',
+//            2 => '排队中',
+//            3 => '检测中',
+//            4 => '检测完成'
+//        ])->dot([
+//            0 => 'danger',
+//            1 => 'info',
+//            2 => 'primary',
+//            3 => 'warning',
+//            4 => 'success',
+//        ]);
+//        $grid->column('title', '标题');
+//        $grid->column('writer', '作者');
+//        $grid->column('words', '字数');
+//        $grid->column('pay_price', '支付金额')->totalRow(function($amount) {
+//
+//            return "<span class='text-danger text-bold'><i class='fa fa-yen'></i> {$amount} 元</span>";
+//
+//        });
+//        $grid->column('pay_type', '支付方式');
+//        $grid->column('from', '来源');
+//        $grid->column('created_at', '创建时间')->sortable();
+//        // 禁用创建按钮，后台不需要创建订单
+//        $grid->disableCreateButton();
         return $grid;
     }
 
