20462f2357529991cf9af87313b0de805388b323
阿沙冲冲冲 <361719196@qq.com>
2020-04-26 17:09:13 +0800

    上传测试

--- resources/views/dev_cnweipu_com/orders/create.blade.php
+++ resources/views/dev_cnweipu_com/orders/create.blade.php
@@ -50,7 +50,7 @@
         </ul>
         <!-- <form action="{{route('orders.store')}}" method="post" id="form"> -->
         <form >
-          @csrf
+          <!-- @csrf -->
           <input type="hidden" name="cid" id="cid" >
           <input type="hidden" name="from" value="万方PC端">
           <input type="hidden" name="file_id" value="" id="hidden_form_id">
@@ -222,6 +222,7 @@
             from: '万方PC端',
             file_id: $("#hidden_form_id").val(),
             type: 'file',
+            content:'',
 title: '撒反倒',
 writer: '手动阀'}).then(res=>{
         console.log(res,3123123)
