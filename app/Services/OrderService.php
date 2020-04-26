c9e667469b6548f0d8997a73fde1e624ced4cc07
Summer <csy13808796047@gmail.com>
2020-04-26 11:43:07 +0800

    文件异步上传:seedling:

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -53,7 +53,7 @@ class OrderService
                 }
             }
             if($words <= $category->min_words && $words >= $category->max_words) {
-                throw new InvalidRequestException("检测字数必须在" . $category->min_words . "与" . $category->max_words . "之间");
+                throw new InvalidRequestException("检测字数必须在" . $category->min_words . "与" . $category->max_words . "之间", 422);
                 return;
             }
             switch ($category->price_type) {
