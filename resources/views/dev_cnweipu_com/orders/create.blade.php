f20d570904a2b741434182585065a3cf92e2f947
阿沙冲冲冲 <361719196@qq.com>
2020-04-26 09:38:52 +0800

    文件上传测试

--- resources/views/dev_cnweipu_com/orders/create.blade.php
+++ resources/views/dev_cnweipu_com/orders/create.blade.php
@@ -52,6 +52,8 @@
           @csrf
           <input type="hidden" name="cid" id="cid">
           <input type="hidden" name="from" value="万方PC端">
+          <input type="hidden" name="form_id" value="" id="hidden_form_id">
+          <input type="hidden" name="type" value="" id="hideen_type">
           <div class="form-group">
             <div class="input-group mt-3">
               <div class="input-group-prepend">
@@ -191,6 +193,10 @@
           }
         }).then(res=>{
           console.log(res,'fsadf')
+          var file_id=res.data.data.id;
+          $("#hidden_form_id").val(file_id);
+          $("#hideen_type").val('file');
+
         }).catch(err=>{
           console.log(err,3213123)
         })
