374f586909b7bfac3aff9b13f71cf81439c5b430
Summer <csy13808796047@gmail.com>
2020-04-26 11:38:44 +0800

    文件异步上传:seedling:

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -21,10 +21,10 @@ class OrderService
         $order = \DB::transaction(function() use ($request) {
             $category = Category::findOrFail($request->cid);
             $user = \Auth()->user();
+            $fileWordsHandler = app(FileWordsHandle::class);
+            $fileUploadHandle = app(FileUploadHandler::class);
+            $wordHandler = app(WordHandler::class);
             if($request->type == 'file') {
-                $fileWordsHandler = app(FileWordsHandle::class);
-                $fileUploadHandle = app(FileUploadHandler::class);
-                $wordHandler = app(WordHandler::class);
                 if($fileId = $request->file_id) {
                     $result = File::find($fileId);
                 }
