c60e25b47577339396c2a0819fad6724d59c3107
阿沙冲冲冲 <361719196@qq.com>
2020-04-27 17:58:46 +0800

    上传测试

--- resources/views/dev_cnweipu_com/orders/create.blade.php
+++ resources/views/dev_cnweipu_com/orders/create.blade.php
@@ -280,7 +280,7 @@
 
               if(res.status==201){
                 var paymsg =res.data.data;
-                $('#paymsg').append(`<div><P>论文题目:${paymsg.title}</P><p>作者：${paymsg.writer}</p><p>价格:${paymsg.price}</p></div>`)
+                $('#paymsg').append(`<div style='width: 602px;border: 1px solid;margin-bottom:20px;'><P>论文题目:${paymsg.title}</P><p>作者：${paymsg.writer}</p><p>价格:${paymsg.price}</p></div>`)
               }
 
             }).catch(err=>{
