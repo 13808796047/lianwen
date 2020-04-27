ae814376b7e72aad7543706edce23295241f55e7
阿沙冲冲冲 <361719196@qq.com>
2020-04-27 19:33:46 +0800

    上传测试

--- resources/views/dev_cnweipu_com/orders/create.blade.php
+++ resources/views/dev_cnweipu_com/orders/create.blade.php
@@ -208,7 +208,7 @@
       //多文件上传
       $('#customFiles').change(function (e) {
         $('#newelement_container').children().remove()
-        $('#paymsg').children().filter('div').remove();
+        $('#paymsg').children().remove();
         //console.log(e,'312312');
         $('#newelement').css('display','block')
         // $('.custom-file-label').html(e.target.files[0].name)
