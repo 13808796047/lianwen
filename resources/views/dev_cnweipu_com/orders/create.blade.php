3523a36d09605554add4c5570bc49f131833910f
阿沙冲冲冲 <361719196@qq.com>
2020-04-27 11:27:11 +0800

    上传测试

--- resources/views/dev_cnweipu_com/orders/create.blade.php
+++ resources/views/dev_cnweipu_com/orders/create.blade.php
@@ -232,6 +232,7 @@
               })
             })
           }).catch(err=>{
+            console.log(err);
             index++;
             $('#progress_bar_line').css("width","100%")
             $('#progress_text').html("不允许上传的文件类型");
