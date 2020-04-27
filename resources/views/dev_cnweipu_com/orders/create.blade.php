2c96026df1b664dc425f53b1940db3796d555765
阿沙冲冲冲 <361719196@qq.com>
2020-04-27 12:40:19 +0800

    上传测试

--- resources/views/dev_cnweipu_com/orders/create.blade.php
+++ resources/views/dev_cnweipu_com/orders/create.blade.php
@@ -225,7 +225,7 @@
             $('#progress_bar_line').html('上传成功')
             $('#progress_text').html("上传成功");
             // alert('上传成功')
-            $("#newelement").append(`<div>订单${index}<input id='title' type='text' name='title' value=${item.name}>论文题目<input type='text' class='titlec' value=${item.name}>论文作者<input type='text' class='authorc' value='匿名'>检测系统<select><option value='1' class='options'>fsda</option></select></div>`);
+            $("#newelement").append(`<div style='margin-bottom:10px'>订单${index}<input id='title' type='text' name='title' value=${item.name}>论文题目<input type='text' class='titlec' value=${item.name}>论文作者<input type='text' class='authorc' value='匿名'>检测系统<select><option value='1' class='options'>fsda</option></select></div>`);
           }).catch(err=>{
             console.log(err);
             index++;
