8083aaddffbe566143df3a3afcd3779bc965afb8
阿沙冲冲冲 <361719196@qq.com>
2020-04-27 13:40:07 +0800

    上传测试

--- resources/views/dev_cnweipu_com/orders/create.blade.php
+++ resources/views/dev_cnweipu_com/orders/create.blade.php
@@ -222,7 +222,7 @@
             for (let i = 0; i < obj.length; i++) {
               var id=value[i].id;
               var name=value[i].name;
-              option+=`<option value='${id[i]}' class='options'>${name[i]}</option>`
+              option+=`<option value=${id} class='options'>${name}</option>`
             }
             var file_id=res.data.data.id;
             set.add(file_id);
