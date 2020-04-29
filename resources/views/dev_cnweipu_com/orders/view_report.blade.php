fb3215a04bcc759fc965c4783652ed07837abc5c
阿沙冲冲冲 <361719196@qq.com>
2020-04-29 14:00:17 +0800

    点击二维码

--- resources/views/dev_cnweipu_com/orders/view_report.blade.php
+++ resources/views/dev_cnweipu_com/orders/view_report.blade.php
@@ -67,9 +67,9 @@
   <script !src="">
     $(function () {
       $("#qrcode").click(function(){
+          console.log({!!$order!!},312312)
           axios.get('{{ route('official_account.index') }}').then(res => {
         swal({
-
           // content 参数可以是一个 DOM 元素，这里我们用 jQuery 动态生成一个 img 标签，并通过 [0] 的方式获取到 DOM 元素
           content: $('<img src="' + res.data.url + '" style="display: block;margin: 0 auto;"/>')[0],
         })
