f107047498bf4046bd084a2978b3c1fe1bdb5ff0
Summer <csy13808796047@gmail.com>
2020-04-29 17:40:07 +0800

    测试:seedling:

--- resources/views/dev_cnweipu_com/orders/qrcode/index.blade.php
+++ resources/views/dev_cnweipu_com/orders/qrcode/index.blade.php
@@ -5,7 +5,7 @@
   <meta name="viewport"
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>论文检测</title>
+  <title>论文检测报告</title>
 </head>
 <body>
 <img style="width: 100vw; height: 100vh; object-fit: contain;" src="{{ $img_url }}" alt="">
