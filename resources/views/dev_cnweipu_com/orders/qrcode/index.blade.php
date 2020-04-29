747b00906cbee2613237aea9fad01a0394dbf278
阿沙冲冲冲 <361719196@qq.com>
2020-04-29 17:52:36 +0800

    上传测试

--- resources/views/dev_cnweipu_com/orders/qrcode/index.blade.php
+++ resources/views/dev_cnweipu_com/orders/qrcode/index.blade.php
@@ -3,13 +3,13 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport"
-        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        content="width=device-width, initial-scale=1.0,initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>论文检测报告</title>
 </head>
 <body>
 <div style="width:100%">
-<img style="width: 100vw; height: 90vh; object-fit: contain;" src="{{ $img_url }}" alt="">
+<img style="width: 100vw; height: 87vh; object-fit: contain;" src="{{ $img_url }}" alt="">
 <p style="text-align: center; padding: 0; margin: 0;">长按图片保存</p>
 </div>
 </body>
