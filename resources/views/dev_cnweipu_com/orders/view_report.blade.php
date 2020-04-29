a2a87eb33401ffb039c01be4750e5e8d597732c8
阿沙冲冲冲 <361719196@qq.com>
2020-04-29 14:44:25 +0800

    重复率判断

--- resources/views/dev_cnweipu_com/orders/view_report.blade.php
+++ resources/views/dev_cnweipu_com/orders/view_report.blade.php
@@ -86,7 +86,7 @@
           let order = {!!$order!!};
           console.log(order,order.rate)
           //判断是否存在重复率
-          if(order.rate){
+          if(order.rate==0){
             $('#exampleModal').modal('show')
           }else{
             axios.get(`/orders/${order.id}/qrcode`).then(res => {
