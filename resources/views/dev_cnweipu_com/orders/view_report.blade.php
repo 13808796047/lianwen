7fef9e081ccc4c0fdcd19b596ab09c1ebf250421
阿沙冲冲冲 <361719196@qq.com>
2020-04-29 17:31:43 +0800

    上传测试

--- resources/views/dev_cnweipu_com/orders/view_report.blade.php
+++ resources/views/dev_cnweipu_com/orders/view_report.blade.php
@@ -35,7 +35,7 @@
             <div class="card-title">
               <h2 class="text-3xl text-center">{{ $order->title }}</h2>
               <div class="info">
-                <span>作者:</span>
+                <!-- <span>作者:</span>
                 &emsp;
                 <span>{{ $order->writer }}</span>
                 &emsp;
@@ -49,14 +49,15 @@
                 &emsp;
                 <a href="{{ route('orders.download',['orderid'=>$order->orderid]) }}"
                    class="bg-blue-500 px-2 rounded-sm text-white">下载报告</a>
-                <span class="bg-blue-500 px-2 rounded-sm text-white" style="margin-left:13px" id="qrcode">生成二维码</span>
-              </div>
-              <!-- <span style="color:red;">注:检查报告系统仅保存10天，请及时下载保存,如需帮助请联系客服微信(查重问题:cx5078,降重帮助:lwcheck)</span>
+                <span class="bg-blue-500 px-2 rounded-sm text-white" style="margin-left:13px" id="qrcode">生成二维码</span> -->
+                <span style="color:red;">注:检查报告系统仅保存10天，请及时下载保存,如需帮助请联系客服微信(查重问题:cx5078,降重帮助:lwcheck)</span>
               <div style="margin-top:10px;">
               <a href="{{ route('orders.download',['orderid'=>$order->orderid]) }}"
                    class="bg-blue-500 px-2 rounded-sm text-white" style="display: inline-block;width: 203px;">下载完整报告</a>
                 <span class="px-2 rounded-sm text-white" style="margin-left:13px;display: inline-block;width: 203px;background:	#32CD32" id="qrcode">生成检测证书</span>
-              </div> -->
+              </div>
+              </div>
+
             </div>
             <!-- Modal -->
 
