e0365ea827951f0362acaacbaa01e9942dd14678
Summer <csy13808796047@gmail.com>
2020-04-29 11:45:51 +0800

    测试:seedling:

--- app/helpers.php
+++ app/helpers.php
@@ -179,7 +179,16 @@ function read_docx($file)
     $content = str_replace('</w:r></w:p>', "\r\n", $content);
 //    $content = preg_replace($pattern, '/^PEVuZE5vdGU(.*?)ADDIN EN.CITE.DATA$/', $content);
     $content = preg_replace('/^PEVuZE5vdGU(.*?)ADDIN EN.CITE.DATA$/', '', $content);
+    $content = preg_replace('/HYPERLINK .l _Toc\d+/', '', $content);
+    $content = preg_replace('/PAGEREF _Toc\d+ /', '', $content);
+
     $striped_content = strip_tags($content);
+
+    preg_match_all('/\s(.*)摘要/', $striped_content, $arr);
+    foreach($arr[1] as $value) {
+        $striped_content = str_replace($value, '', $striped_content);
+    }
+//    $striped_content = preg_replace('/HYPERLINK\S*l _Toc\d+\S/', '', $striped_content);
 
     return $striped_content;
 }
@@ -206,41 +215,42 @@ function autowrap($fontsize, $angle, $fontface, $string, $width)
 function docx2html($source)
 {
     $phpWord = \PhpOffice\PhpWord\IOFactory::load($source);
-    $content = '';
+    $html = '';
     foreach($phpWord->getSections() as $section) {
         foreach($section->getElements() as $ele1) {
-            $paragraphStyle = $ele1->getParagraphStyle();
-            if($paragraphStyle) {
-                $content .= '<p style="text-align:' . $paragraphStyle->getAlignment() . ';text-indent:20px;">';
-            } else {
-                $content .= '\n';
-            }
+//            $paragraphStyle = $ele1->getParagraphStyle();
+//            if($paragraphStyle) {
+//                $html .= '<p style="text-align:' . $paragraphStyle->getAlignment() . ';text-indent:20px;">';
+//            } else {
+//                $html .= '<p>';
+//            }
             if($ele1 instanceof \PhpOffice\PhpWord\Element\TextRun) {
                 foreach($ele1->getElements() as $ele2) {
                     if($ele2 instanceof \PhpOffice\PhpWord\Element\Text) {
                         $style = $ele2->getFontStyle();
-//                        $fontFamily = mb_convert_encoding($style->getName(), 'GBK', 'UTF-8');
-//                        $fontSize = $style->getSize();
-//                        $isBold = $style->isBold();
-//                        $styleString = '';
-//                        $fontFamily && $styleString .= "font-family:{$fontFamily};";
-//                        $fontSize && $styleString .= "font-size:{$fontSize}px;";
-//                        $isBold && $styleString .= "font-weight:bold;";
-//                        $html .= sprintf('<span style="%s">%s</span>',
-//                            $styleString,
-//
-//                        );
-                        $content .= mb_convert_encoding($ele2->getText(), 'GBK', 'UTF-8');
+                        $fontFamily = mb_convert_encoding($style->getName(), 'GBK', 'UTF-8');
+                        $fontSize = $style->getSize();
+                        $isBold = $style->isBold();
+                        $styleString = '';
+                        $fontFamily && $styleString .= "font-family:{$fontFamily};";
+                        $fontSize && $styleString .= "font-size:{$fontSize}px;";
+                        $isBold && $styleString .= "font-weight:bold;";
+                        $html .= sprintf('<span style="%s">%s</span>',
+                            $styleString,
+                            mb_convert_encoding($ele2->getText(), 'GBK', 'UTF-8')
+                        );
                     } elseif($ele2 instanceof \PhpOffice\PhpWord\Element\Image) {
-//                        $imageSrc = 'images/' . md5($ele2->getSource()) . '.' . $ele2->getImageExtension();
-//                        $imageData = $ele2->getImageStringData(true);
-//                        // $imageData = 'data:' . $ele2->getImageType() . ';base64,' . $imageData;
-//                        file_put_contents($imageSrc, base64_decode($imageData));
-//                        $html .= '<img src="' . $imageSrc . '" style="width:100%;height:auto">';
+                        $imageSrc = 'images/' . md5($ele2->getSource()) . '.' . $ele2->getImageExtension();
+                        $imageData = $ele2->getImageStringData(true);
+                        // $imageData = 'data:' . $ele2->getImageType() . ';base64,' . $imageData;
+                        file_put_contents($imageSrc, base64_decode($imageData));
+                        $html .= '<img src="' . $imageSrc . '" style="width:100%;height:auto">';
                     }
                 }
             }
+            $html .= '</p>';
         }
     }
-    return mb_convert_encoding($content, 'UTF-8', 'GBK');
+
+    return mb_convert_encoding($html, 'UTF-8', 'GBK');
 }
