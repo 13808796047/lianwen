b90981e80b05d2300314a62e8abed719e613e7b2
阿沙冲冲冲 <361719196@qq.com>
2020-05-06 16:00:51 +0800

    alert弹框

--- resources/views/dev_cnweipu_com/orders/create.blade.php
+++ resources/views/dev_cnweipu_com/orders/create.blade.php
@@ -351,9 +351,8 @@
           $('#tosubmit').attr("disabled", false);
           // alert('上传成功')
           $('#alertbot').modal('show')
+          setTimeout("$('#alertbot').modal('hide')",2000);
           oneid = res.data.data.id;
-          setTimeout("$('#alertbot').modal('hide')",2000);//5s延时自动关闭
-         });
         }).catch(err => {
           alert('上传失败，仅支持docx和txt格式，最大支持15M')
           $('#tosubmit').attr("disabled", true);
