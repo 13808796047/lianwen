8eb037a5f5cca355114346ec2a1d7515fffa44be
Summer <csy13808796047@gmail.com>
2020-05-06 16:43:19 +0800

    后台样式:seedling:

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -2,57 +2,35 @@
 
 namespace App\Admin\Controllers;
 
+use App\Admin\Metrics\Examples;
 use App\Http\Controllers\Controller;
-use App\Models\Category;
-use App\Models\Order;
-use Encore\Admin\Controllers\Dashboard;
-use Encore\Admin\Form\Tab;
-use Encore\Admin\Grid;
-use Encore\Admin\Layout\Column;
-use Encore\Admin\Layout\Content;
-use Encore\Admin\Layout\Row;
-use Encore\Admin\Widgets\Box;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+use Dcat\Admin\Controllers\Dashboard;
+use Dcat\Admin\Layout\Column;
+use Dcat\Admin\Layout\Content;
+use Dcat\Admin\Layout\Row;
 
 class HomeController extends Controller
 {
-    //订单统计grid
-//    public function orderTotalGrid()
-//    {
-//        $grid = new Grid(new Category());
-//        $grid->model()->with(['orders' => function($query) use ($request) {
-//            $query->withOrder($request->date);
-//        }])->get();
-//        return $grid;
-//    }
-
-    public function index(Request $request, Content $content)
+    public function index(Content $content)
     {
-        $classOrders = Category::query()->with(['orders' => function($query) use ($request) {
-            $query->withOrder($request->date);
-        }])->get();
-//        $totalOrders = Category::query()->with(['orders' => function($query) use ($request) {
-//            $query->withOrder('created_at', $request->date);
-//        }])->get();
-        $sourceOrders = Order::query()->withOrder($request->date)->get()->groupBy('from');
-//        $sourceTotalOrders = Order::query()->withOrder('created_at', $request->date)->get()->groupBy('from');
         return $content
-            ->title('首页')
-            ->view('admin.home.index', [
-                'class_orders' => $classOrders,
-//                'total_orders' => $totalOrders,
-                'source_orders' => $sourceOrders,
-//                'source_total_orders' => $sourceTotalOrders,
-            ]);
-    }
+            ->header('Dashboard')
+            ->description('Description...')
+            ->body(function (Row $row) {
+                $row->column(6, function (Column $column) {
+                    $column->row(Dashboard::title());
+                    $column->row(new Examples\Tickets());
+                });
 
-    public function orderStatistics(Request $request)
-    {
-        $orders = Order::withOrder($request->input('date', 'today'))
-            ->selectRaw("cid,count(*) as total_order,count('date_pay') as total_pay_order,sum('pay_price') as total_price")
-            ->groupBy('cid')
-            ->paginate();
-        return response()->json($orders);
+                $row->column(6, function (Column $column) {
+                    $column->row(function (Row $row) {
+                        $row->column(6, new Examples\NewUsers());
+                        $row->column(6, new Examples\NewDevices());
+                    });
+
+                    $column->row(new Examples\Sessions());
+                    $column->row(new Examples\ProductOrders());
+                });
+            });
     }
 }
--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -1,22 +1,17 @@
 <?php
 
 use Illuminate\Routing\Router;
+use Illuminate\Support\Facades\Route;
+use Dcat\Admin\Admin;
 
 Admin::routes();
 
 Route::group([
-    'prefix' => config('admin.route.prefix'),
-    'namespace' => config('admin.route.namespace'),
-    'middleware' => config('admin.route.middleware'),
-], function(Router $router) {
-    $router->get('/', 'HomeController@index')->name('admin.home.index');
-    $router->get('order_statistics', 'HomeController@orderStatistics')->name('admin.home.statis');
-    $router->get('users', 'UsersController@index');
-    $router->resource('categories', CategoriesController::class);
-    $router->resource('orders', OrdersController::class)->only(['index', 'show', 'edit']);
-    $router->post('orders/{order}/receved', 'OrdersController@receved')->name('admin.orders.receved');
+    'prefix'        => config('admin.route.prefix'),
+    'namespace'     => config('admin.route.namespace'),
+    'middleware'    => config('admin.route.middleware'),
+], function (Router $router) {
 
-    $router->get('orders/{order}/download_paper', 'OrdersController@downloadPaper')->name('admin.orders.download_paper');
-    $router->get('orders/{order}/download_report', 'OrdersController@downloadReport')->name('admin.orders.download_report');
-    $router->post('repeat_check/{order}', 'OrdersController@repeatCheck')->name('admin.orders.repeat_check');
+    $router->get('/', 'HomeController@index');
+
 });
