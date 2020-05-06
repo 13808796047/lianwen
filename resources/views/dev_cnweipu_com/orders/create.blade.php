2bae15bbca21be56c4577bf7ffab23dec8252c8f
阿沙冲冲冲 <361719196@qq.com>
2020-05-06 15:13:53 +0800

    alert弹框

--- resources/views/dev_cnweipu_com/orders/create.blade.php
+++ resources/views/dev_cnweipu_com/orders/create.blade.php
@@ -18,11 +18,11 @@
 @stop
 @section('content')
   <!-- alert弹框 -->
-  <div class="toast" role="alert" aria-live="assertive" aria-atomic="true" id='alertbot'>
+  <div role="alert" aria-live="assertive" aria-atomic="true" class="toast" id="alertbot">
   <div class="toast-header">
     <img src="..." class="rounded mr-2" alt="...">
     <strong class="mr-auto">Bootstrap</strong>
-    <small class="text-muted">11 mins ago</small>
+    <small>11 mins ago</small>
     <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
       <span aria-hidden="true">&times;</span>
     </button>
