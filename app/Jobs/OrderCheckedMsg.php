1ba279916bdf2884aaf5dbd3c173a752d63e4896
Summer <csy13808796047@gmail.com>
2020-05-07 16:48:18 +0800

    优化队列:seedling:

--- app/Jobs/OrderCheckedMsg.php
+++ app/Jobs/OrderCheckedMsg.php
@@ -25,25 +25,25 @@ class OrderCheckedMsg implements ShouldQueue
     public function handle()
     {
         if($this->order->user->weixin_openid && $this->order->status == 4) {
-            return;
+            $data = [
+                'first' => '您的论文已经检测完成,点击查看结果',
+                'keyword1' => ['value' => $this->order->title, 'color' => '#173177'],
+                'keyword2' => ['value' => $this->order->category->name, 'color' => '#173177'],
+                'keyword3' => ['value' => $this->order->rate, 'color' => '#173177'],
+                'keyword4' => ['value' => $this->order->created_at->format("Y-m-d H:i:s"), 'color' => '#173177'],
+                'remark' => ['value' => '点击查看详情！', 'color' => '#173177']
+            ];
+            app('official_account')->template_message->send([
+                'touser' => $user->weixin_openid,
+                'template_id' => 'IKyhivjep0fGj-oaCRdfLBRkRSSvESl5lRUQVCXOM2o',
+                'url' => 'https://wanfang.lianwen.com',
+                'miniprogram' => [
+                    'appid' => 'wx6340d7d2fead020b',
+                    'pagepath' => 'pages/lookup/lookup',
+                ],
+                'data' => $data,
+            ]);
         }
-        $data = [
-            'first' => '您的论文已经检测完成,点击查看结果',
-            'keyword1' => ['value' => $this->order->title, 'color' => '#173177'],
-            'keyword2' => ['value' => $this->order->category->name, 'color' => '#173177'],
-            'keyword3' => ['value' => $this->order->rate, 'color' => '#173177'],
-            'keyword4' => ['value' => $this->order->created_at->format("Y-m-d H:i:s"), 'color' => '#173177'],
-            'remark' => ['value' => '点击查看详情！', 'color' => '#173177']
-        ];
-        app('official_account')->template_message->send([
-            'touser' => $user->weixin_openid,
-            'template_id' => 'IKyhivjep0fGj-oaCRdfLBRkRSSvESl5lRUQVCXOM2o',
-            'url' => 'https://wanfang.lianwen.com',
-            'miniprogram' => [
-                'appid' => 'wx6340d7d2fead020b',
-                'pagepath' => 'pages/lookup/lookup',
-            ],
-            'data' => $data,
-        ]);
+
     }
 }
