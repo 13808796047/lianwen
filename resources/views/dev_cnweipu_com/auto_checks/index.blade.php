87a29718d100dac6adda9ac48e3277bb83dd0407
阿沙冲冲冲 <361719196@qq.com>
2020-05-09 15:07:33 +0800

    测试服

--- resources/views/dev_cnweipu_com/auto_checks/index.blade.php
+++ resources/views/dev_cnweipu_com/auto_checks/index.blade.php
@@ -89,7 +89,7 @@
       //点击降重
       $('#reduce').click(function(){
         let words =  $('#words span').text();
-        let contents = $('#content').html();
+        let contents = $('#content').val();
         console.log(words,contents,31)
         if(words>5000){
           alert('字数大于5000')
