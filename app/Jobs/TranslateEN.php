ac5e7dd273ce1fcfbfbc9e0fcef19cb71cf9d1a1
Summer <csy13808796047@gmail.com>
2020-05-11 10:12:49 +0800

    后台批量启动队列:seedling:

--- app/Jobs/TranslateEN.php
+++ app/Jobs/TranslateEN.php
@@ -27,7 +27,7 @@ class TranslateEN implements ShouldQueue
     {
         $result = app(AutoCheckHandler::class)->translate_en($this->autoCheck->content_before);
         if($result['trans_result'][0]['dst']) {
-            dispatch(new TranslateCN($this->autoCheck))->delay(now()->addSeconds(3));
+            dispatch(new TranslateCN($this->autoCheck))->delay(now()->addSeconds(1));
         }
     }
 }
