c8d2f911fdbf5c7a3e5e3a3ad732a0a0fe7d7bea
阿沙冲冲冲 <361719196@qq.com>
2020-05-12 17:01:45 +0800

    test

--- resources/views/dev_cnweipu_com/recharges/index.blade.php
+++ resources/views/dev_cnweipu_com/recharges/index.blade.php
@@ -66,7 +66,7 @@
 								</td>
 							</tr>
 						</table>
-						<a type="button" id="bottonsubmit" style="height:33px; margin-left:20px; margin-left:320px;" href="#"
+						<a type="button" id="bottonsubmit" style="height:33px; margin-left:20px; margin-left:320px;" href="javascript:;"
 						 class="btn btn-primary btn-sm sbtn">提交</a>
 						<a type="button" id="btn-wechat" style="height:33px; margin-left:20px; margin-left:320px;display: none" href="javascript:;"
 						 class="btn btn-primary btn-sm sbtn">提交</a>
