c5a08c873f1430a2ce502d249a8bfb5a3a503794
Summer <csy13808796047@gmail.com>
2020-05-12 17:24:02 +0800

    百度翻译:seedling:

--- app/Http/Controllers/RechargesController.php
+++ app/Http/Controllers/RechargesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Recharge;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class RechargesController extends Controller
 {
@@ -15,5 +16,19 @@ class RechargesController extends Controller
     public function show(Recharge $recharge)
     {
         return view('domained::recharges.show', compact('recharge'));
+    }
+
+    public function store(Request $request)
+    {
+        $user = $request->user();
+        $recharge = DB::transaction(function() use ($user, $request) {
+            $recharge = new Recharge([
+                'total_amount' => $request->total_amount,
+            ]);
+            $recharge->user()->associate($user);
+            $recharge->save();
+            return $recharge;
+        });
+        return redirect()->route('domained::recharges.show', compact('recharge'));
     }
 }
