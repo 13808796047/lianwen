77e231114098702ed5cee0dd412a5904b5ff6468
阿沙冲冲冲 <361719196@qq.com>
2020-05-13 14:38:54 +0800

    test

--- resources/views/dev_cnweipu_com/orders/show.blade.php
+++ resources/views/dev_cnweipu_com/orders/show.blade.php
@@ -125,9 +125,7 @@
 								</td>
 							</tr>
 						</table>
-						<!-- <a type="button" id="bottonsubmit" style="height:33px; margin-left:20px; margin-left:320px;" href="{{ route('payments.alipay', ['order' => $order->id])}}"
-						 class="btn btn-primary btn-sm sbtn">提交</a> -->
-             <a type="button" id="bottonsubmit" style="height:33px; margin-left:20px; margin-left:320px;" href="javascript:;" target="_blank"
+						<a type="button" id="bottonsubmit" style="height:33px; margin-left:20px; margin-left:320px;" href="{{ route('payments.alipay', ['order' => $order->id])}}"
 						 class="btn btn-primary btn-sm sbtn">提交</a>
 						<a type="button" id="btn-wechat" style="height:33px; margin-left:20px; margin-left:320px;display: none" href="javascript:;"
 						 class="btn btn-primary btn-sm sbtn">提交</a>
@@ -185,12 +183,6 @@
           })
       });
     });
-    $('#bottonsubmit').click(function(){
-       let order= {!!$order!!};
-       console.log(order)
-      // /payments/7/alipay/recharge
-      location.href=`/payments/${order.id}/alipay/recharge`
-     })
 
     // $(function () {
     //   setInterval("checkpaied()", 1000);
