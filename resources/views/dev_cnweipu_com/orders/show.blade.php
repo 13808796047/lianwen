3114db00c82d149dbcf4c5b55bb36a651aeeaca0
阿沙冲冲冲 <361719196@qq.com>
2020-05-13 15:15:03 +0800

    test

--- resources/views/dev_cnweipu_com/orders/show.blade.php
+++ resources/views/dev_cnweipu_com/orders/show.blade.php
@@ -125,8 +125,7 @@
 								</td>
 							</tr>
 						</table>
-						<!-- <a type="button" id="bottonsubmit" style="height:33px; margin-left:20px; margin-left:320px;" href="{{ route('payments.alipay', ['order' => $order->id])}}"
-						 class="btn btn-primary btn-sm sbtn">提交</a> -->
+
              <a type="button" id="bottonsubmit" style="height:33px; margin-left:20px; margin-left:320px;" href="javascript:;" target="_blank"
 						 class="btn btn-primary btn-sm sbtn">提交</a>
 						<a type="button" id="btn-wechat" style="height:33px; margin-left:20px; margin-left:320px;display: none" href="javascript:;"
