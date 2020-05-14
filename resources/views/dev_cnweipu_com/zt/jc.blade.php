17416142169032e618c0b7728957495c6c9d29e5
阿沙冲冲冲 <361719196@qq.com>
2020-05-14 16:55:07 +0800

    test

--- resources/views/dev_cnweipu_com/zt/jc.blade.php
+++ resources/views/dev_cnweipu_com/zt/jc.blade.php
@@ -1,4 +1,72 @@
-@section('styles')
-<div>
-  fdshfjsafkjsdaflafsdkfasjk
-</div>
+<section class="foraml-box register" id="web">
+    <div style="margin:0 auto;">
+      <p style="font-size:123px;text-align:center;font-weight:bold;color:#fbf0a6">立即注册送查重次数</p>
+    </div>
+    <div class="envelope" style="padding:10px 100px">
+      <div style="display:flex;justify-content: center;align-items: center;">
+        <p style="background:#4876FF;font-size:50px;color:#fff;padding: 10px 40px;margin-right:90px;">PC一键注册</p>
+        <div style="text-align:center;">
+
+          <img
+            src="https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket=gQGR8DwAAAAAAAAAAS5odHRwOi8vd2VpeGluLnFxLmNvbS9xLzAyQUxyNFp4RVFjeTIxUlhBQWh1YzIAAgR717xeAwQAjScA"
+            style="width:300px;height:300px;">
+          <p style="line-height: 50px;font-size: 16px;color: #7b3015;">微信扫码注册</p>
+        </div>
+      </div>
+    </div>
+    <p class="t">简单三步<span></span>即送代金券</p>
+    <div class="card" style="padding: 60px 0 50px;">
+      <p>1、分享给好友</p>
+      <p>2、好友成功注册</p>
+      <p>3、双方各获得各5次自动降重次数</p>
+    </div>
+    <p class="t">推荐注册<span></span>活动规则</p>
+    <div class="card" style="text-align:left;padding: 40px 40px;">
+      <p>1、每推荐1名好友成功注册（绑定微信或手机号码），推荐人/注册人各获得10元无门槛代金券；代金券获得数量无上限</p>
+      <p>2、代金券有效期为1个月；</p>
+      <p>3、代金券不能提现，仅用于购买5118服务时消费抵扣，无门槛代金券最多可叠加10张抵扣；</p>
+      <p>4、注册过程中使用了多人的推荐链接，以最后使用的推荐链接为准；</p>
+      <p>5、对不符合活动规则的问题数据，5118有权撤销推荐和对应的代金券。</p>
+      <p style="text-align:right;margin-top:40px;">活动最终解释权归5118官方</p>
+    </div>
+    <br>
+    <br>
+  </section>
+  <div id="app" style="display: none;">
+    <section>
+      <div class="bk register">
+        <div class="envelope">
+          <div style="display:flex;padding:30vw 10vw 0;justify-content: center;color:#fbf0a6">
+            <p style="font-size:6.7vw;font-weight:bold;">立即注册，送查重次数</p>
+          </div>
+          <div class="line"></div>
+          <div style="text-align:center">
+            <img
+              src="https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket=gQFW8TwAAAAAAAAAAS5odHRwOi8vd2VpeGluLnFxLmNvbS9xLzAyYnZaN1lHRVFjeTIxU1lNQTF1Y0UAAgS847xeAwQAjScA"
+              style="width:35vw;height:35vw;">
+            <p>微信扫码分享</p>
+            <p style="line-height: 3vw;font-size: 2.8vw;">(可长按二维码自动识别)</p>
+          </div>
+        </div>
+        <div>
+          <p style="color: #fbf0a6;text-align:center;margin:7vw 0;font-size:6vw;">—— 简单三步送查重次数 ——</p>
+        </div>
+        <div class="card" style="padding: 5vw 8vw;text-align:left;font-weight:100">
+          <p>1、分享给好友</p>
+          <p>2、好友成功注册</p>
+          <p>3、双方各获得各5次自动降重次数</p>
+        </div>
+
+        <div>
+          <p style="color: #fbf0a6;text-align:center;margin:7vw 0;font-size:6vw;">—— 推荐注册活动规则 ——</p>
+        </div>
+        <div class="card" style="padding: 5vw 8vw;text-align:left;font-weight:100">
+          <p>1、每推荐1名好友成功注册（微信登录或绑定手机号），推荐人和注册人各获得5次自动降重次数，获得的次数可叠加，无上限；</p>
+          <p>2、所获得降重次数不可提现，仅用于使用自动降重服务时抵扣；</p>
+          <p>3、严禁使用非法手段获取，对于问题账号本站有权撤销相应数据或封禁账号。</p>
+        </div>
+
+        <p style="color: #fbf0a6;text-align:center;padding:5vw 0">活动最终解释权归5118官方</p>
+      </div>
+    </section>
+  </div>
