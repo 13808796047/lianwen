c459f44141f3ccbd762f19c59a2891b6a4b781fa
阿沙冲冲冲 <361719196@qq.com>
2020-05-14 10:05:42 +0800

    test

--- resources/views/dev_cnweipu_com/auto_checks/index.blade.php
+++ resources/views/dev_cnweipu_com/auto_checks/index.blade.php
@@ -77,7 +77,19 @@
   <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true" id="tjModal">
   <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
     <div class="modal-content">
-      ...
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLongTitle">提示</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body" style="text-align:center;">
+          <p>推荐注册，赠送自动降重次数</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">取消</button>
+          <button type="button" class="btn btn-primary" id="sureshop">确定</button>
+        </div>
     </div>
   </div>
 </div>
