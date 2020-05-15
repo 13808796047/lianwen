172c61b24b110cc1e23374e97a0f52bb099a58b2
阿沙冲冲冲 <361719196@qq.com>
2020-05-15 11:24:12 +0800

    qrode

--- resources/views/dev_cnweipu_com/zt/jc.blade.php
+++ resources/views/dev_cnweipu_com/zt/jc.blade.php
@@ -5,6 +5,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>Document</title>
 </head>
 <style>
@@ -157,6 +159,7 @@
 
   </div>
 </body>
+<script src="{{ mix('js/app.js') }}"></script>
 <script type="text/javascript" src="{{ asset('asset/js/qrcode.min.js') }}"></script>
 <script>
 
