911ca3567eae6043d4bd65e2ee2d0c71730b8240
Summer <csy13808796047@gmail.com>
2020-05-15 14:20:40 +0800

    批量生成降重次数:seedling:

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\URL;
+
 class PagesController extends Controller
 {
     public function index()
     {
+        dd(URL::previous());
         return view('domained::pages.index');
     }
 }
