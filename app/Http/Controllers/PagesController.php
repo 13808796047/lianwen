c9691a6a46e72538e52dea91f69290af391b58aa
Summer <csy13808796047@gmail.com>
2020-05-15 14:44:56 +0800

    批量生成降重次数:seedling:

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -12,8 +12,7 @@ class PagesController extends Controller
         //以下为测试
         //在搜索引擎搜索个关键词，进入网站
         $word = search_word_from(URL::previous());
-        dd($word);
-        if(!empty($word['keyword'])) {
+        if(!empty($word['from'])) {
             dd('关键字：' . $word['keyword'] . ' 来自：' . $word['from']);
         }
         return view('domained::pages.index');
