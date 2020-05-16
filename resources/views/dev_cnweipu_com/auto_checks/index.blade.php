d0d05c61c21736598f5ec35825797571fc9b7303
阿沙冲冲冲 <361719196@qq.com>
2020-05-16 09:45:05 +0800

    test

--- resources/views/dev_cnweipu_com/auto_checks/index.blade.php
+++ resources/views/dev_cnweipu_com/auto_checks/index.blade.php
@@ -15,9 +15,20 @@
 @stop
 @section('content')
   <!-- alert提示框 -->
-  <div class="alert alert-danger">
-    <strong>降重失败，请重试</strong>
+  <div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+    <div class="toast-header">
+      <img src="..." class="rounded mr-2" alt="...">
+      <strong class="mr-auto">Bootstrap</strong>
+      <small>11 mins ago</small>
+      <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+    <div class="toast-body">
+      Hello, world! This is a toast message.
+    </div>
   </div>
+</div>
   <!-- 模态框 -->
   <div class="modal fade bd-example-modal-sm" id="exampleModal" tabindex="-1" role="dialog"
     aria-labelledby="exampleModalLabel" aria-hidden="true">
