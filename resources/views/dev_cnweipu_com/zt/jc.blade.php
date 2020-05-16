c913bcbc852f1245d808bd2eda0deb2753fd9193
阿沙冲冲冲 <361719196@qq.com>
2020-05-16 16:19:53 +0800

    test

--- resources/views/dev_cnweipu_com/zt/jc.blade.php
+++ resources/views/dev_cnweipu_com/zt/jc.blade.php
@@ -7,8 +7,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>推荐活动</title>
-  <link rel="stylesheet" href="{{ asset('asset/css/bootstrap.css') }}">
+  <title>推荐s活动</title>
+  <!-- <link rel="stylesheet" href="{{ asset('asset/css/bootstrap.css') }}"> -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
 <style>
   .foraml-box.register {
