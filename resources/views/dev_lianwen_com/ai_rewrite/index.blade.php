19f3b590337e840b5f7df516e32a3d8d94310de6
阿沙冲冲冲 <361719196@qq.com>
2020-05-18 17:41:08 +0800

    test

---
+++ resources/views/dev_lianwen_com/ai_rewrite/index.blade.php
@@ -0,0 +1,333 @@
+@extends('domained::layouts.app')
+@section('title','智能查重')
+@section('styles')
+  <!-- <link href="https://css.lianwen.com/css/public_c.css?v=2018v1" type="text/css" rel="stylesheet"/>
+  <link href="https://css.lianwen.com/css/index_2017.css" type="text/css" rel="stylesheet"/> -->
+  <!-- <link rel="stylesheet" href="{{asset('asset/css/index.css')}}"> -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" rel="stylesheet" />
+  <link rel="stylesheet" href="{{asset('asset/css/check.css')}}">
+  <style>
+    .curfont {
+      font-size: 16px;
+    }
+    del { background: #FF4040; }
+    ins { background: #00ff21;text-decoration:none; }
+  </style>
+@stop
+@section('content')
+  <!-- alert提示框 -->
+
+  <!-- 模态框 -->
+  <div class="modal fade bd-example-modal-sm" id="exampleModal" tabindex="-1" role="dialog"
+    aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-sm" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLongTitle">提示</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body" style="text-align:center;">
+          <p>本次操作将消耗1次降重次数</p>
+          <p>剩余次数：{{ auth()->user()->jc_times}}<span style="color:#4876FF;margin-left:10px;" id="addjctimes">增加次数</span></p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">取消</button>
+          <button type="button" class="btn btn-primary" id="surecheck">确定</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- 模态框结束 -->
+   <!-- 模态框2 -->
+   <div class="modal fade bd-example-modal-sm" id="beingModal" tabindex="-1" role="dialog"
+    aria-labelledby="exampleModalLabel" aria-hidden="true" data-backdrop="static">
+    <div class="modal-dialog modal-dialog-centered modal-sm" role="document">
+      <div class="modal-content">
+        <div class="modal-body" style="text-align:center;">
+          <div style="padding:20px 0">正在降重中，请勿刷新页面</div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- 模态框2结束-->
+  <!-- 购买降重字数模态框 -->
+  <div class="modal fade bd-example-modal-sm" id="jctimeModal" tabindex="-1" role="dialog"
+    aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-sm" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLongTitle">提示</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body" style="text-align:center;">
+          <p>购买自动降重次数</p>
+          <p style="margin: 6px 0;font-size: 11px;color: #F4A460;">(价格:1元/次)</p>
+          <p>请输入购买次数<span style="padding:0 10px;" id="cutjctime">-</span><span style="border: 1px solid;padding: 3px;" id="curjctime">1</span><span style="padding:0 10px;" id="addjctime">+</span></p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">取消</button>
+          <button type="button" class="btn btn-primary" id="sureshop">确定</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- 购买降重字数模态框结束 -->
+  <!-- 推荐弹框 -->
+  <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true" id="tjModal">
+  <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+    <div class="modal-content">
+        <div class="modal-header" style="padding:5px 10px;border-bottom:0;">
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body" style="padding:0;text-align:center;">
+          <p style="font-weight: bold;font-size: 18px;">推荐注册，赠送自动降重次数</p>
+          <p style="font-size: 13px;color: #FFA54F;">双方各送5次</p>
+          <p style="margin:10px 0;">方式1：分享地址，邀请好友注册（适合电脑此操作）</p>
+          <div style="display:flex;justify-content: center;">
+            <p style="border: 1px solid;padding: 0 10px;">https://dev.cnweipu.com/zt/jc?uid={{auth()->user()->id}}</p>
+            <p style="margin-left: 10px;background: red;color: #fff;padding: 0 10px;">复制链接</p>
+          </div>
+          <p style="margin:10px 0;">方式2：微信扫码，邀请好友注册（适合手机操作）</p>
+          <div style="display:flex;justify-content: center;" id="qrcode">
+
+          </div>
+          <p style="font-size: 13px;margin-top: 5px;">微信扫码分享</p>
+        </div>
+        <div class="modal-footer" style="border:none;display:block;font-size:13px;">
+          <p>活动规则</p>
+          <p>1、每推荐1名好友成功注册（微信登录或绑定手机号），推荐人和注册人各获得5次自动降重次数，获得的次数可叠加，无上限；</p>
+          <p>2、所获得降重次数不可提现，仅用于使用自动降重服务时抵扣；</p>
+          <p>3、严禁使用非法手段获取，对于问题账号本站有权撤销相应数据或封禁账号。</p>
+        </div>
+    </div>
+  </div>
+</div>
+  <!-- 推荐弹框结束 -->
+    <!--左边导航-->
+    <div class="main clearfix" id="jcafter">
+      <div class="lbox fl">
+      <p style="font-size: 20px;">请输入你要降重的内容。<span style="font-size:16px;color:#757575;">（最大支持5000字）</span></p>
+      <textarea name="content" id="content"
+        style="width:97%;height: 500px;padding:20px;box-sizing:border-box;font-size:20px;outline: none;border:1px solid #ddd;margin-top:20px"></textarea>
+      <p style="float: right;font-size: 13px;padding-right: 30px;" id="words">当前输入<span>0</span>字</p>
+      <p style="background-color: #4876FF;display: inline;padding: 5px 20px;color:#fff;text-align: center;font-size:15px;"
+        id="reduce">
+        一键降重</p>
+      <div style="display: flex;justify-content: center;margin-top: 15px;">
+      <p>剩余次数:<span id="jc_time">{{ auth()->user()->jc_times}}</span></p><span style="color:#4876FF;margin-left: 10px;" id="shopjctime">增加次数</span>
+    </div>
+    </div>
+    <!--右边内容-->
+    <div class="rbox fr" style="min-height:900px;">
+      <div class="box">
+        <b>1、检测结果是否准确？</b>
+        <p>如果你们学校也是用万方检测，那结果是一致的。同一个的系统、同样的比对库、同样的算法，所以只要在本系统提交的内容和学校的一致，那检测结果是一致的。</p>
+        <b>2、检测需要多少时间？</b>
+        <p>正常情况，万方检测需要10分钟左右，高峰期可能会延迟，但不会超过1个小时，如果长时间未出结果请联系客服微信：cx5078解决。</p>
+        <b>3、论文上传之后安全吗？</b>
+        <p>本系统有明确的条文规定并遵守严格的论文保密规定，对所有用户提交的送检文档仅做检测分析，绝不保留全文，承诺对用户送检的文档不做任何形式的收录和泄露。</p>
+        <b>4、提交以后能不能退款？</b>
+        <p>此系统一旦提交，系统开始检测后，即产生消费，无法退款！</p>
+        <b>5、检测内容范围？</b>
+        <p>系统不检测文章中的封面、致谢、学校(需要替换成"X")等个人信息，请在提交前自己删除，若提交后由系统自动删除时出现的任何问题责任自负！</p>
+        <b>6、检测时作者需要填吗？</b>
+        <p>在提交检测的文章中，引用了一些内以前自己所写的内容并且被小论文系统文献库收录，需要在此次检测中排除这些；则会有“去除本人已发表文献复制比”的结果。</p>
+      </div>
+    </div>
+    </div>
+    <!-- 降重成功后 -->
+    <div style="padding: 21px; display: none;min-height:800px;" id="jclater">
+    <div style="display: flex;">
+      <!-- <div style="width: 50%;">
+        <p style="font-size: 17px;font-weight: bold;">降重前</p>
+        <textarea name="" id="content_after"
+          style="width:100%;height: 550px;box-sizing: border-box;overflow-x:visible;border:1px solid #ccc;outline: none;"></textarea>
+      </div>
+      <div style="width: 50%;">
+        <p style="font-size: 17px;font-weight: bold;">降重后</p>
+        <textarea name="" id="content_later" style="width: 100%;height: 550px;border: none;outline: none;border:1px solid #ccc"
+          readonly></textarea>
+      </div> -->
+      <table style="width:100%;">
+        <tr>
+            <td style="width:48%;">
+              <div style="font-size:19px;font-weight:bold;margin-left:10px;">降重前</div>
+              <div style="height:650px;overflow-y:auto;background:#fff;border: 1px solid #ddd;padding: 19px;margin-right:5px;" id="content_after">
+              </div>
+            </td>
+            <td style="width:48%;">
+              <div style="font-size:19px;font-weight:bold;margin-left:10px;">降重后</div>
+              <div style="height:650px;overflow-y:auto;background:#fff;border: 1px solid #ddd;padding: 19px;margin-left:5px;" id="content_later">
+              </div>
+            </td>
+        </tr>
+      </table>
+    </div>
+    <p style="font-size: 13px;margin-top: 10px;text-align: center;">
+      注：本工具是通过运用AI技术对原文进行降重，结果仅供参考，需要稍作调整让语句更通顺。如需高质量人工降重请联系微信：13878811985
+    </p>
+    <p style="background-color: #4876FF;padding: 5px 20px;color:#fff;text-align: center;margin:0 auto;width:100px;margin-top:16px;" id="againjc">
+      再来一篇</p>
+    <div style="display: flex;justify-content: center;margin-top: 15px;">
+      <p>剩余次数:<span id="jc_time"></span></p><span style="color:#4876FF;margin-left: 10px;" id="shopjctime">增加次数</span>
+    </div>
+  </div>
+@stop
+@section('scripts')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+<script type="text/javascript" src="{{ asset('asset/js/qrcode.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('asset/js/diff.js') }}"></script>
+  <script>
+    $(() => {
+      $('.navbar>div').removeClass('container').addClass('container-fluid')
+      $('#headerlw').addClass('curfont')
+      $('#tjModal').modal('show')
+      //生成分享二维码
+      var qrcode = document.getElementById('qrcode')
+
+      /*也可以配置二维码的宽高等*/
+       var qrcodeObj = new QRCode('qrcode', {
+          text: 'http://dev.cnweipu.com/zt/jc?uid='+{{auth()->user()->id}},
+          width: 120,
+          height: 120,
+          colorDark: '#000000', //前景色
+          colorLight: '#ffffff',//背景色
+          correctLevel: QRCode.CorrectLevel.H
+      })
+      console.log({{auth()->user()->id}},1313123123123123123)
+      //获取字数
+      $("#content").bind('input',(e)=>{
+        $('#words span').html(e.target.value.length)
+      })
+      //增加降重次数
+      $("#shopjctime").click(function(){
+        $("#jctimeModal").modal('show')
+      })
+      $("#addjctimes").click(function(){
+        $('#exampleModal').modal('hide')
+        $("#jctimeModal").modal('show')
+      })
+      //点击增加降重次数
+      $("#addjctime").click(function(){
+        let current = Number($("#curjctime").text())+1;
+        $("#curjctime").text(current)
+
+      })
+      //确认购买
+      $("#sureshop").click(function(){
+        let totalprice=$("#curjctime").text();
+        console.log(totalprice,3131)
+        axios.post('{{ route('recharges.store') }}',{
+          total_amount:totalprice,
+          amount:totalprice
+        }).then(res => {
+          console.log(res,312312)
+          let number = res.data.data.amount;
+          let id =res.data.data.id;
+          let price=res.data.data.total_amount;
+          location.href=`/recharges/${id}`
+        }).catch(err => {
+          console.log(err,31312)
+        })
+      })
+      })
+      //点击减少降重次数
+      $("#cutjctime").click(function(){
+        let current = Number($("#curjctime").text());
+        if(current==1) return;
+        let cur =current-1;
+        $("#curjctime").text(cur)
+      })
+      //点击降重
+      $('#reduce').click(function(){
+        let words =  $('#words span').text();
+        let contents = $('#content').val();
+        console.log(words,contents,31)
+        if(words>5000){
+          alert('字数大于1000')
+          return
+        }
+        $('#exampleModal').modal('show')
+      })
+      //再来一篇
+      $('#againjc').click(function(){
+        window.location.reload()
+      })
+       //对比diff方法
+       function changed(a,b,c) {
+            var oldContent = a
+            var content1 = b
+            var diff = JsDiff['diffChars'](oldContent, content1);
+      var arr = new Array();
+      for (var i = 0; i < diff.length; i++) {
+        if (diff[i].added && diff[i + 1] && diff[i + 1].removed) {
+          var swap = diff[i];
+          diff[i] = diff[i + 1];
+          diff[i + 1] = swap;
+        }
+        console.log(diff[i]);
+        var diffObj = diff[i];
+        var content = diffObj.value;
+
+        //可以考虑启用，特别是后台清理HTML标签后的文本
+        if (content.indexOf("\n") >= 0) {
+
+          var reg = new RegExp('\n', 'g');
+          content = content.replace(reg, '<br/>');
+        }
+        if (diffObj.removed) {
+          arr.push('<del title="删除的部分">' + content + '</del>');
+        } else if (diffObj.added) {
+          arr.push('<ins title="新增的部分">' + content + '</ins>');
+        } else {
+          //没有改动的部分
+          arr.push('<span title="没有改动的部分">' + content + '</span>');
+        }
+      }
+          var html = arr.join('');
+          document.getElementById('content_after').innerHTML = html;
+
+          document.getElementById('content_later').innerHTML = c;
+        }
+       //点击确认显示正在降重弹框
+      $("#surecheck").click(function () {
+        $('#exampleModal').modal('hide')
+        $('#beingModal').modal('show')
+        let num = 3;
+        togetJc(num)
+      })
+
+      function togetJc(num){
+        let contents = $('#content').val();
+        axios.post('{{ route('ai_rewrite.store') }}',{content:contents})
+          .then(res => {
+            console.log(res,1323122321)
+            $('#beingModal').modal('hide')
+            $('#jcafter').css('display', 'none')
+            var htmlstring=res.data.result.new_content;
+            var stringtemp =htmlstring.replace(/<[^>]+>/g, "");
+            changed(contents,stringtemp,htmlstring)
+            $('#jc_time').html(res.data.user.jc_times)
+            $("#jclater").css('display', 'block')
+          })
+          .catch(err =>{
+            num--;
+            if(num>=0){
+              togetJc(num)
+              return;
+            }else{
+              $('#beingModal').modal('hide')
+              toastr.error('降重失败，请重试');
+            }
+          }
+          );
+      }
+
+  </script>
+@stop
---
+++ resources/views/dev_lianwen_com/zt/jc.blade.php
@@ -0,0 +1,349 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <title>推荐活动</title>
+  <link rel="stylesheet" href="{{ asset('asset/css/bootstrap4.css') }}">
+
+</head>
+<style>
+  .foraml-box.register {
+    background: url(https://s0.5118img.com/assist/images/formal/invite/bk_4.jpg?v=d5596a1a6d) top no-repeat, url(https://s0.5118img.com/assist/images/formal/invite/bk_4.jpg?v=d5596a1a6d) center 380px no-repeat;
+    background-color: #f54c15;
+  }
+
+  .bk.register {
+    background: url(https://s0.5118img.com/assist/images/formal/invite/bk_4.jpg?v=d5596a1a6d) no-repeat #f44b12;
+    background-color: #f54c15;
+    background-size: 100% auto;
+  }
+
+  * {
+    padding: 0;
+    margin: 0;
+  }
+  html, body {
+    min-height: 100%;
+  }
+
+  .foraml-box {
+    padding: 150px 0 1px;
+  }
+
+  .foraml-box.register .envelope {
+
+    width: 901px;
+    height: 480px;
+    margin: 0 auto;
+    text-align: center;
+  }
+
+  .foraml-box .t {
+    font-size: 36px;
+    color: #fbf0a6;
+    text-align: center;
+    margin: 80px 0 35px;
+  }
+
+  .cards {
+    margin: 0 auto;
+    width: 880px;
+    font-family: SourceHanSansCN-Normal;
+    background-color: #fff3e3;
+    border-radius: 16px;
+    text-align: center;
+    font-size: 16px;
+    line-height: 40px;
+    color: #7b3015;
+  }
+
+  #app {
+    width: 100%;
+    height: 100%;
+  }
+
+  .bk.register .envelope .line {
+    height: 1px;
+    border-bottom: 1px solid #ccc;
+    width: 60vw;
+    margin: 5vw auto;
+  }
+
+  .bk .card {
+    margin: 0 auto;
+    width: 80%;
+    font-family: SourceHanSansCN-Normal;
+    background-color: #fff3e3;
+    border-radius: 3vw;
+    text-align: center;
+    font-size: 5vw;
+    color: #7b3015;
+  }
+
+  .modalcontainer {
+    margin: 0 auto;
+  }
+
+  .modalcontainer-div {
+    display: flex;
+    width: 80%;
+    align-items: center;
+    margin: 0 auto;
+    height: 50px;
+  }
+
+  .modalcontainer-p {
+    width: 20%;
+    text-align: right;
+    margin-left: 30px;
+    margin-bottom: 0;
+  }
+
+  .modalcontainer-input {
+    width: 77%;
+    margin-left: 10px;
+  }
+</style>
+
+<body>
+  <div class="foraml-box register" id="web">
+   <!-- 二维码弹窗 -->
+ <div class="modal fade " tabindex="-1" role="dialog" aria-labelledby="registerModalLabel" aria-hidden="true" id="registerModel" >
+			<div class="modal-dialog modal-dialog-centered" role="document">
+				<div class="modal-content">
+					<div class="modal-header" style="border-bottom: none;padding-top: 0;padding-bottom: 0;">
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+						</button>
+					</div>
+					<div>
+						<div class="alert alert-danger" role="alert" id="message" style="display: none"></div>
+						<p style="font-size:23px;font-weight:bold;text-align:center;">注册</p>
+
+						<div class="modalcontainer">
+							<div class="modalcontainer-div">
+								<p class="modalcontainer-p">手机号</p>
+								<input type="text" name="phone" id="phone" class="modalcontainer-input">
+							</div>
+							<div class="modalcontainer-div">
+								<p class="modalcontainer-p">密码</p>
+								<input type="text" id="password" class="modalcontainer-input">
+							</div>
+							<div class="modalcontainer-div">
+								<p class="modalcontainer-p">确认密码</p>
+								<input type="text" id="password-confirm" class="modalcontainer-input">
+							</div>
+							<div class="modalcontainer-div">
+								<p class="modalcontainer-p">验证码</p>
+								<input type="text" style="width: 130px;" id="code" class="modalcontainer-input">
+								<!-- <span  id="yzm">发送验证码</span> -->
+								<input type="button" id="yzm" value="获取验证码" style="font-size: 14px;line-height: 20px;height:30px;background:#7CCD7C;color:#fff;padding:0 20px;width:120px;
+		                outline: none;border: 0;"
+								 class="modalcontainer-input">
+							</div>
+						</div>
+						<div style="display: flex;justify-content: center;">
+							<span style="text-align: center;background-color: #4876FF;color: #fff;padding: 7px 13px;margin: 15px 0;" id='submitBtn'>立即注册</span>
+						</div>
+					</div>
+				</div>
+			</div>
+	</div>
+  <!-- 二维码弹窗结束 -->
+    <p style="font-size: 123px;text-align: center;font-weight: bold;color: #fbf0a6;">立即查重 送降重次数</p>
+    <div style="padding: 20px;display: flex;justify-content: center;">
+      <p style="text-align: center;background: #4876FF;font-size: 50px;color:#fff;padding: 10px 30px;letter-spacing:20px"
+        id="tjregister">
+        一键注册</p>
+    </div>
+    <div>
+      <p class="t">简单三步送降重次数</p>
+      <div class="cards">
+        <p>1.分享给好友</p>
+        <p>2.好友成功注册</p>
+        <p>3.双方各获得5次自动降重次数</p>
+      </div>
+    </div>
+    <div style="padding-bottom: 150px;">
+      <p class="t">推荐注册活动规则</p>
+      <div class="cards">
+        <p>1、分享给好友</p>
+        <p>2.好友成功注册</p>
+        <p>双方各获得5次自动降重次数</p>
+      </div>
+    </div>
+  </div>
+  <div id="app" style="display: none;">
+    <section>
+      <div class="bk register">
+        <div class="envelope">
+          <div style="display:flex;padding:30vw 10vw 0;justify-content: center;color:#fbf0a6">
+            <p style="font-size:6.7vw;font-weight:bold;">立即注册，送查重次数</p>
+          </div>
+          <div class="line"></div>
+          <div style="text-align:center">
+            <img
+              src=""
+              style="width:35vw;height:35vw;"
+              id="qrimg">
+            <p>微信扫码分享</p>
+            <p style="line-height: 3vw;font-size: 2.8vw;">(可长按二维码自动识别)</p>
+          </div>
+        </div>
+        <div>
+          <p style="color: #fbf0a6;text-align:center;margin:7vw 0;font-size:6vw;">—— 简单三步送查重次数 ——</p>
+        </div>
+        <div class="card" style="padding: 5vw 8vw;text-align:left;font-weight:100">
+          <p>1、分享给好友</p>
+          <p>2、好友成功注册</p>
+          <p>3、双方各获得各5次自动降重次数</p>
+        </div>
+
+        <div>
+          <p style="color: #fbf0a6;text-align:center;margin:7vw 0;font-size:6vw;">—— 推荐注册活动规则 ——</p>
+        </div>
+        <div class="card" style="padding: 5vw 8vw;text-align:left;font-weight:100">
+          <p>1、每推荐1名好友成功注册（微信登录或绑定手机号），推荐人和注册人各获得5次自动降重次数，获得的次数可叠加，无上限；</p>
+          <p>2、所获得降重次数不可提现，仅用于使用自动降重服务时抵扣；</p>
+          <p>3、严禁使用非法手段获取，对于问题账号本站有权撤销相应数据或封禁账号。</p>
+        </div>
+
+        <p style="color: #fbf0a6;text-align:center;padding:5vw 0;margin-bottom:0;">活动最终解释权归联文检测</p>
+      </div>
+    </section>
+
+  </div>
+
+</body>
+<script src="https://cdn.bootcdn.net/ajax/libs/axios/0.19.2/axios.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<script type="text/javascript" src="{{ asset('asset/js/qrcode.min.js') }}"></script>
+<script>
+
+    !function () {
+    var devices = ["iPhone", "Android", "Windows Phone"]
+    var ua = window.navigator.userAgent
+    for (var i = 0; i < devices.length; i++) {
+      if (ua.indexOf(devices[i]) != -1) {
+        console.log(1)
+        document.getElementById('app').style.display = "block";
+        document.getElementById('web').style.display = "none";
+        return;
+      } else {
+        console.log(2)
+        document.getElementById('app').style.display = "none";
+        document.getElementById('web').style.display = "block";
+
+      }
+    }
+  }()
+  // 二维码
+  var search = window.location.search
+  var id = getSearchString('uid', search);
+  function getSearchString(key, Url) {
+    var str = Url;
+    str = str.substring(1, str.length); // 获取URL中?之后的字符（去掉第一位的问号）
+    // 以&分隔字符串，获得类似name=xiaoli这样的元素数组
+    var arr = str.split("&");
+    var obj = new Object();
+    // 将每一个数组元素以=分隔并赋给obj对象
+    for (var i = 0; i < arr.length; i++) {
+        var tmp_arr = arr[i].split("=");
+        obj[decodeURIComponent(tmp_arr[0])] = decodeURIComponent(tmp_arr[1]);
+    }
+    return obj[key];
+  }
+  console.log(id,312312)
+  axios.get('/invit_official?uid='+id).then(res => {
+       console.log(res,313131311331)
+       document.getElementById("qrimg").src = res.data.url
+
+  })
+
+  var wait = 60;
+  var verification_key = '';
+
+  function time(o) {
+    console.log(o, 123)
+    if (wait == 0) {
+      o.removeAttribute("disabled");
+      o.value = "点击获取验证码";
+      wait = 60;
+    } else {
+      o.setAttribute("disabled", true);
+      o.value = "重新发送(" + wait + ")";
+      wait--;
+      setTimeout(function () {
+        time(o)
+      },
+        1000)
+    }
+  }
+  $('#tjregister').click(function(){
+    console.log(1112)
+    $('#registerModel').modal('show')
+  })
+
+  $('#yzm').click(function () {
+    getcode(this)
+  })
+  function getcode(index) {
+    index.setAttribute("disabled", true);
+    var phone = $("input[name='phone']").val();
+    var reg = /^1[3456789]\d{9}$/;
+    if (!reg.test(phone)) {
+      index.removeAttribute("disabled");
+      $("input[name='phone']").focus();
+      $('#message').show();
+      $('#message').html('<strong>请输入正确的手机号码</strong>');
+      return;
+    }
+    axios.post('https://dev.lianwen.com/api/v1/verificationCodes', {
+      phone: phone,
+    }).then(res => {
+      // swal('验证码已发送成功!,请注意查收!')
+      time(index);
+      verification_key = res.data.key;
+    }).catch(err => {
+      index.removeAttribute("disabled");
+      if (err.response.status == 422) {
+        $('#message').show();
+        $.each(err.response.data.errors, (field, errors) => {
+          $('#message').html('<strong>' + errors[0] + '</strong>');
+        })
+      }
+    })
+  }
+
+  $('#submitBtn').click(() => {
+    axios.post('{{route('register')}}', {
+      'verification_key': verification_key,
+      'phone': $('#phone').val(),
+      'password': $('#password').val(),
+      'password_confirmation': $('#password-confirm').val(),
+      'verification_code': $('#code').val(),
+      'uid':id
+    }).then(res => {
+      alert("注册成功")
+      $('#registerModel').modal('hide')
+      location.href="https://dev.lianwen.com"
+    }).catch(err => {
+      if (err.response.status == 422) {
+      $('#message').show();
+      $.each(err.response.data.errors, (field, errors) => {
+        $('#message').append('<strong>' + errors + '</strong> </br>');
+      })
+      }
+    })
+  })
+
+
+</script>
+
+</html>
