aa4544a9ea26099a5804a2de18154cb9b6ae84b7
阿沙冲冲冲 <361719196@qq.com>
2020-05-18 15:27:07 +0800

    除去注释

--- resources/views/dev_cnweipu_com/pages/index.blade.php
+++ resources/views/dev_cnweipu_com/pages/index.blade.php
@@ -671,12 +671,12 @@
           }
         }).catch(function(err){
           if (err.response.status == 422) {
-            $.each(err.response.data.errors, (field, errors) => {
+            $.each(err.response.data.errors, function(field, errors){
               swal("提示", errors[0]);
             })
           }
           if (err.response.status == 401) {
-            $.each(err.response.data, (field, errors) => {
+            $.each(err.response.data, function(field, errors){
               swal("提示", errors);
             })
           }
