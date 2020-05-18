475cbc8bad521bad23599ef25e16a248b5523c13
Summer <csy13808796047@gmail.com>
2020-05-18 14:16:46 +0800

    批量生成降重次数:seedling:

--- resources/views/admin/home/index.blade.php
+++ resources/views/admin/home/index.blade.php
@@ -94,20 +94,21 @@
           </thead>
           <tbody>
           @foreach($source_orders as $source=> $order)
+            {{ $order }}
             <tr>
               <td>{{ $source }}</td>
               @switch(request()->date)
                 @case('yesterday')
-                <td>{{$order->count().'/'.\App\Models\Order::whereBetween('created_at',[\Carbon\Carbon::now()->subDay()->startOfDay(), \Carbon\Carbon::now()->subDay()->endOfDay()])->count()}}</td>
+                <td>{{$order->count().'/'.\App\Models\Order::whereBetween('created_at',[\Carbon\Carbon::now()->subDay()->startOfDay(), \Carbon\Carbon::now()->subDay()->endOfDay()])->groupBy('from')->count()}}</td>
                 @break
                 @case('month')
-                <td>{{$order->count().'/'.\App\Models\Order::whereBetween('created_at',[\Carbon\Carbon::now()->startOfMonth(), \Carbon\Carbon::now()->endOfMonth()])->count()}}</td>
+                <td>{{$order->count().'/'.\App\Models\Order::whereBetween('created_at',[\Carbon\Carbon::now()->startOfMonth(), \Carbon\Carbon::now()->endOfMonth()])->groupBy('from')->count()}}</td>
                 @break
                 @case('pre_month')
-                <td>{{$order->count().'/'.\App\Models\Order::whereBetween('created_at',[\Carbon\Carbon::now()->subMonth()->startOfMonth(), \Carbon\Carbon::now()->subMonth()->endOfMonth()])->count()}}</td>
+                <td>{{$order->count().'/'.\App\Models\Order::whereBetween('created_at',[\Carbon\Carbon::now()->subMonth()->startOfMonth(), \Carbon\Carbon::now()->subMonth()->endOfMonth()])->groupBy('from')->count()}}</td>
                 @break
                 @default
-                <td>{{$order->count().'/'.\App\Models\Order::whereBetween('created_at',[\Carbon\Carbon::now()->startOfDay(), \Carbon\Carbon::now()->endOfDay()])->count()}}</td>
+                <td>{{$order->count().'/'.\App\Models\Order::whereBetween('created_at',[\Carbon\Carbon::now()->startOfDay(), \Carbon\Carbon::now()->endOfDay()])->groupBy('from')->count()}}</td>
               @endswitch
               <td>{{@number_format($order->count()/(\App\Models\Order::query()->whereNotNull('date_pay')->count()) *100,2)}}
                 %
