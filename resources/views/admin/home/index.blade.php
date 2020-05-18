e593f2b016e2abf1a54e4f29da624ffbd16a608b
Summer <csy13808796047@gmail.com>
2020-05-18 15:57:23 +0800

    批量生成降重次数:seedling:

--- resources/views/admin/home/index.blade.php
+++ resources/views/admin/home/index.blade.php
@@ -54,7 +54,13 @@
               <td>{{ $order->name }}</td>
               @switch(request()->date)
                 @case('yesterday')
-                <td>{{$order->orders->count().'/'.\App\Models\Order::whereBetween('created_at',[\Carbon\Carbon::now()->subDay()->startOfDay(), \Carbon\Carbon::now()->subDay()->endOfDay()])->where('cid',$order->id)->count()}}</td>
+                @php
+                  $todayData = $order->orders->count().'/'.\App\Models\Order::whereBetween('created_at',[\Carbon\Carbon::now()->subDay()->startOfDay(), \Carbon\Carbon::now()->subDay()->endOfDay()])->where('cid',$order->id)->count()
+                @endphp
+                <td>{{$todayData}}</td>
+                <td>{{@number_format(eval($todayData),2)}}
+                  %
+                </td>
                 @break
                 @case('month')
                 <td>{{$order->orders->count().'/'.\App\Models\Order::whereBetween('created_at',[\Carbon\Carbon::now()->startOfMonth(), \Carbon\Carbon::now()->endOfMonth()])->where('cid',$order->id)->count()}}</td>
@@ -65,9 +71,7 @@
                 @default
                 <td>{{$order->orders->count().'/'.\App\Models\Order::whereBetween('created_at',[\Carbon\Carbon::now()->startOfDay(), \Carbon\Carbon::now()->endOfDay()])->where('cid',$order->id)->count()}}</td>
               @endswitch
-              <td>{{@number_format(($order->orders->count()/(\App\Models\Order::query()->whereNotNull('date_pay')->count()) *100),2)}}
-                %
-              </td>
+
               <td>{{ $order->orders->sum('pay_price') }}元</td>
             </tr>
 
