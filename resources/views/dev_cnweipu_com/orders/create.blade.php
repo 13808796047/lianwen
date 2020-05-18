947de32041d0a2d46eb123cbaa5fe91bec865bfb
阿沙冲冲冲 <361719196@qq.com>
2020-05-18 14:33:29 +0800

    test

--- resources/views/dev_cnweipu_com/orders/create.blade.php
+++ resources/views/dev_cnweipu_com/orders/create.blade.php
@@ -267,6 +267,7 @@
         $(this).siblings().children('i').removeClass('selected')
         $(this).children('i').addClass('selected')
         $('#cid').val($(this).data('id'))
+        console.log( $('#cid').val($(this).data('id')),12312)
       })
       $('#content').bind('input propertychange', (e) => {
         $('#words span').html(e.target.value.length)
