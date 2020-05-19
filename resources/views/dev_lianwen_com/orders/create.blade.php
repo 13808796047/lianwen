a69d00e94b9a798eed5d132d9ba23ddf3e3453dd
阿沙冲冲冲 <361719196@qq.com>
2020-05-19 14:02:03 +0800

    test

--- resources/views/dev_lianwen_com/orders/create.blade.php
+++ resources/views/dev_lianwen_com/orders/create.blade.php
@@ -241,26 +241,26 @@
     $('#content').bind('input propertychange', (e) => {
       $('#words span').html(e.target.value.length)
     })
-     //时间选择
-     $('#element_id')[0].dataset.startDate = '2000/1/1'
-      $('#element_id')[0].dataset.endDate = getNowFormatDate()
-      $('#element_id').cxCalendar();
-      function getNowFormatDate() {
-         var date = new Date();
-         var seperator1 = "-";
-         var year = date.getFullYear();
-         var month = date.getMonth() + 1;
-         var strDate = date.getDate();
-         if (month >= 1 && month <= 9) {
-           month = "0" + month;
-         }
-         if (strDate >= 0 && strDate <= 9) {
-           strDate = "0" + strDate;
-         }
-         var currentdate = year + seperator1 + month + seperator1 + strDate
-         return currentdate;
-      }
-      //时间选择结束
+    //  //时间选择
+    //  $('#element_id')[0].dataset.startDate = '2000/1/1'
+    //   $('#element_id')[0].dataset.endDate = getNowFormatDate()
+    //   $('#element_id').cxCalendar();
+    //   function getNowFormatDate() {
+    //      var date = new Date();
+    //      var seperator1 = "-";
+    //      var year = date.getFullYear();
+    //      var month = date.getMonth() + 1;
+    //      var strDate = date.getDate();
+    //      if (month >= 1 && month <= 9) {
+    //        month = "0" + month;
+    //      }
+    //      if (strDate >= 0 && strDate <= 9) {
+    //        strDate = "0" + strDate;
+    //      }
+    //      var currentdate = year + seperator1 + month + seperator1 + strDate
+    //      return currentdate;
+    //   }
+    //   //时间选择结束
     //单文件上传
     $('#customFile').change(function (e) {
       $('.custom-file-label').html(e.target.files[0].name)
