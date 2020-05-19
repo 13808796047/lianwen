eadb9850388e5187ade376d995c59d1acc98f118
阿沙冲冲冲 <361719196@qq.com>
2020-05-19 10:28:43 +0800

    test

--- resources/views/dev_lianwen_com/ai_rewrite/index.blade.php
+++ resources/views/dev_lianwen_com/ai_rewrite/index.blade.php
@@ -90,8 +90,8 @@
           <p style="font-size: 13px;color: #FFA54F;">双方各送5次</p>
           <p style="margin:10px 0;">方式1：分享地址，邀请好友注册（适合电脑此操作）</p>
           <div style="display:flex;justify-content: center;">
-            <p style="border: 1px solid;padding: 0 10px;">https://dev.lianwen.com/zt/jc?uid={{auth()->user()->id}}</p>
-            <p style="margin-left: 10px;background: red;color: #fff;padding: 0 10px;">复制链接</p>
+            <p style="border: 1px solid;padding: 0 10px;" id="urladdress">https://dev.lianwen.com/zt/jc?uid={{auth()->user()->id}}</p>
+            <p style="margin-left: 10px;background: red;color: #fff;padding: 0 10px;" id="copyurl">复制链接</p>
           </div>
           <p style="margin:10px 0;">方式2：微信扫码，邀请好友注册（适合手机操作）</p>
           <div style="display:flex;justify-content: center;" id="qrcode">
@@ -219,6 +219,16 @@
         $("#curjctime").text(current)
 
       })
+      //复制链接
+      var urladd = $("#urladdress").html();
+      var clipboard = new Clipboard('#copyurl', {
+        text: function () {
+        return urladd;
+        }
+      });
+      clipboard.on('success', function (e) {
+        alert("复制成功");
+      });
       //确认购买
       $("#sureshop").click(function(){
         let totalprice=$("#curjctime").text();
