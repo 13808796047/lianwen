497d05cbf6623eb39c24e671e33dc7e589ec20e9
阿沙冲冲冲 <361719196@qq.com>
2020-05-21 15:41:06 +0800

    test

--- resources/views/dev_cnweipu_com/pages/index.blade.php
+++ resources/views/dev_cnweipu_com/pages/index.blade.php
@@ -279,6 +279,27 @@
     </a>
   </div>
   <div class="clearfix"></div>
+  <!-- 绑定手机号 -->
+  <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true" id='badingphone'>
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalCenterTitle">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+  <!-- 绑定手机号结束 -->
   <!----- //End-slider---->
   <!----start-services---->
   <div id="ser" class="services">
@@ -625,8 +646,8 @@
       @auth
       let isphone ={{auth()->user()->phone}}
       console.log(isphone,312312)
-      if(!isphone){
-        console.log('xixi',1323)
+      if(isphone){
+        $('#badingphone').modal('show')
       }
       @endauth
 
