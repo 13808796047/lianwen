8b33c022ed362b412afb7af83373e1d07b724e30
阿沙冲冲冲 <361719196@qq.com>
2020-05-21 15:49:40 +0800

    test

--- resources/views/dev_cnweipu_com/pages/index.blade.php
+++ resources/views/dev_cnweipu_com/pages/index.blade.php
@@ -290,7 +290,16 @@
         </button>
       </div>
       <div class="modal-body">
-        ...
+        <form>
+          <div class="form-group">
+            <label for="recipient-name" class="col-form-label">手机号:</label>
+            <input type="text" class="form-control" id="recipient-name">
+          </div>
+          <div class="form-group">
+            <label for="message-text" class="col-form-label">验证码:</label>
+            <input type="text" class="form-control" id="recipient-name">
+          </div>
+        </form>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
