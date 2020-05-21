920e111e16a85945de25e1a63d3e0bbc4dbc32b3
Summer <csy13808796047@gmail.com>
2020-05-21 17:28:41 +0800

    批量生成降重次数:seedling:

--- database/migrations/2020_05_21_164053_add_dev_weixin_openid_to_users_table.php
+++ database/migrations/2020_05_21_164053_add_dev_weixin_openid_to_users_table.php
@@ -15,9 +15,13 @@ class AddDevWeixinOpenidToUsersTable extends Migration
     {
         Schema::table('users', function(Blueprint $table) {
             $table->string('dev_weixin_openid')->unique()->nullable();
+            $table->string('dev_weixin_unionid')->unique()->nullable();
             $table->string('wf_weixin_openid')->unique()->nullable();
-            $table->string('wp_weixin_openid')->unique()->nullable();
-            $table->string('pp_weixin_openid')->unique()->nullable();
+            $table->string('wf_weixin_unionid')->unique()->nullable();
+            $table->string('weipu_weixin_openid')->unique()->nullable();
+            $table->string('weipu_weixin_unionid')->unique()->nullable();
+            $table->string('paperpass_weixin_openid')->unique()->nullable();
+            $table->string('paperpass_weixin_unionid')->unique()->nullable();
         });
     }
 
@@ -30,6 +34,13 @@ class AddDevWeixinOpenidToUsersTable extends Migration
     {
         Schema::table('users', function(Blueprint $table) {
             $table->dropColumn('dev_weixin_openid');
+            $table->dropColumn('dev_weixin_unionid');
+            $table->dropColumn('wf_weixin_openid');
+            $table->dropColumn('wf_weixin_unionid');
+            $table->dropColumn('weipu_weixin_openid');
+            $table->dropColumn('weipu_weixin_unionid');
+            $table->dropColumn('paperpass_weixin_openid');
+            $table->dropColumn('paperpass_weixin_unionid');
         });
     }
 }
