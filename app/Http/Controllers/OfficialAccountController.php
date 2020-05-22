fb2f88c523969571c255a30f76641d8838cbc66a
Summer <csy13808796047@gmail.com>
2020-05-22 16:19:38 +0800

    微信小程序登录:seedling:

--- app/Http/Controllers/OfficialAccountController.php
+++ app/Http/Controllers/OfficialAccountController.php
@@ -179,6 +179,7 @@ class OfficialAccountController extends Controller
                 default:
                     $attributes['cn_weixin_openid'] = $wxUser['openid'];
             }
+            info($loginUser);
             if(!$loginUser) {
                 $loginUser = User::create($attributes);
             } else {
