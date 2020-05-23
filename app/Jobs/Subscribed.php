071f746e353fbce951bbecec3c813e44ac100eb7
Summer <csy13808796047@gmail.com>
2020-05-23 10:23:57 +0800

    微信小程序登录:seedling:

--- app/Jobs/Subscribed.php
+++ app/Jobs/Subscribed.php
@@ -30,54 +30,48 @@ class Subscribed implements ShouldQueue
      */
     public function handle()
     {
-        $data = [
+        $send['data'] = [
             'first' => '您未绑定手机号，绑定手机号后可接收网站订单状态。',
             'keyword1' => ['value' => $this->user->nick_name, 'color' => '#173177'],
             'keyword2' => ['value' => '未绑定', 'color' => '#173177'],
             'remark' => ['value' => '绑定手机号！', 'color' => '#173177']
         ];
-        info($this->officialAccount);
+
+//        'touser' => $send['touser'],
+//        'template_id' => $send['template_id'],
+////                'url' => 'https://wap.lianwen.com/bading?openid=' . $this->order->user->weixin_openid,
+//        'miniprogram' => [
+//            'appid' => $send['miniprogram']['appid'],
+//            'pagepath' => $send['miniprogram']['pagepath'],
+//        ],
+
         switch ($this->officialAccount) {
             case 'gh_192a416dfc80':
-                $openid = $this->user->dev_weixin_openid;
-                $template_id = config('wechat.official_account.dev.templates.subscribed.template_id');
-                $appid = config('wechat.official_account.dev.templates.subscribed.appid');
-                $pagePath = config('wechat.official_account.dev.templates.subscribed.page_path');
-                info([$template_id, $appid, $pagePath], ['dev']);
+                $send['touser'] = $this->user->dev_weixin_openid;
+                $send['template_id'] = config('wechat.official_account.dev.templates.subscribed.template_id');
+                $send['miniprogram']['appid'] = config('wechat.official_account.dev.templates.subscribed.appid');
+                $send['miniprogram']['pagepath'] = config('wechat.official_account.dev.templates.subscribed.page_path');
                 break;
             case 'gh_caf405e63bb3':
-                $openid = $this->user->wf_weixin_openid;
-                $template_id = config('wechat.official_account.wf.templates.subscribed.template_id');
-                $appid = config('wechat.official_account.wf.templates.subscribed.appid');
-                $pagePath = config('wechat.official_account.wf.templates.subscribed.page_path');
-                info([$template_id, $appid, $pagePath], ['wf']);
+                $send['touser'] = $this->user->wf_weixin_openid;
+                $send['template_id'] = config('wechat.official_account.wf.templates.subscribed.template_id');
+                $send['miniprogram']['appid'] = config('wechat.official_account.wf.templates.subscribed.appid');
+                $send['miniprogram']['pagepath'] = config('wechat.official_account.wf.templates.subscribed.page_path');
                 break;
             case 'gh_192a416dfc80':
-                $openid = $this->user->wp_weixin_openid;
-                $template_id = config('wechat.official_account.wp.templates.subscribed.template_id');
-                $appid = config('wechat.official_account.wp.templates.subscribed.appid');
-                $pagePath = config('wechat.official_account.wp.templates.subscribed.page_path');
-                info([$template_id, $appid, $pagePath], ['wp']);
+                $send['touser'] = $this->user->wp_weixin_openid;
+                $send['template_id'] = config('wechat.official_account.wp.templates.subscribed.template_id');
+                $send['miniprogram']['appid'] = config('wechat.official_account.wp.templates.subscribed.appid');
+                $send['miniprogram']['pagepath'] = config('wechat.official_account.wp.templates.subscribed.page_path');
                 break;
             default:
-                $openid = $this->user->cn_weixin_openid;
-                $template_id = config('wechat.official_account.cn.templates.subscribed.template_id');
-                $appid = config('wechat.official_account.cn.templates.subscribed.appid');
-                $pagePath = config('wechat.official_account.cn.templates.subscribed.page_path');
+                $send['touser'] = $this->user->cn_weixin_openid;
+                $send['template_id'] = config('wechat.official_account.cn.templates.subscribed.template_id');
+                $send['miniprogram']['appid'] = config('wechat.official_account.cn.templates.subscribed.appid');
+                $send['miniprogram']['pagepath'] = config('wechat.official_account.cn.templates.subscribed.page_path');
         }
-        info($openid, [$template_id, $appid, $pagePath]);
-        if($openid) {
-            $send = [
-                'touser' => $openid,
-                'template_id' => $template_id,
-//                'url' => 'https://wap.lianwen.com/bading?openid=' . $this->order->user->weixin_openid,
-                'miniprogram' => [
-                    'appid' => $appid,
-                    'pagepath' => $pagePath,
-                ],
-                'data' => $data,
-            ];
-            info($send, [123123123]);
+        info($send);
+        if($send['touser']) {
             app('official_account')->template_message->send($send);
         }
     }
