21461637d0b613f20cbc8d63f46916f3d31b6d91
阿沙冲冲冲 <361719196@qq.com>
2020-05-25 15:26:26 +0800

    test

--- resources/views/dev_lianwen_com/layouts/app.blade.php
+++ resources/views/dev_lianwen_com/layouts/app.blade.php
@@ -1,6 +1,14 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
-
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?8c167fa6441cd7b5d0a1cb99cccf9fe8";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
