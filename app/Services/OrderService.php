e491deff03a1787646b32e43507baf1d137e4d85
Summer <csy13808796047@gmail.com>
2020-05-25 16:25:29 +0800

    注册增加查重次数:seedling:

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -92,7 +92,7 @@ class OrderService
             if($user->is_free && $category->id == 12) {
                 if($user->dev_weixin_openid || $user->dev_weapp_openid) {
                     $new_price = $price - 3;
-                    $new_price < 0 ? 0 : $new_price;
+                    if($new_price < 0) $new_price = 0;
                     $order->price = $new_price;
                 }
             }
