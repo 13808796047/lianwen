901a3b07c7c405450d2f316d3b886f5c7e396fee
Summer <csy13808796047@gmail.com>
2020-05-25 16:26:35 +0800

    注册增加查重次数:seedling:

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -89,7 +89,7 @@ class OrderService
                 'keyword' => $referer['keyword']
             ]);
             $order->user()->associate($user);
-            if($user->is_free && $category->id == 12) {
+            if($user->is_free && $category->id == 1) {
                 if($user->dev_weixin_openid || $user->dev_weapp_openid) {
                     $new_price = $price - 3;
                     if($new_price < 0) $new_price = 0;
