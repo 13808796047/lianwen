d25256de9d8e016ace66fb614b579ec104b932c4
阿沙冲冲冲 <361719196@qq.com>
2020-05-25 17:17:30 +0800

    test

--- resources/views/dev_lianwen_com/orders/show.blade.php
+++ resources/views/dev_lianwen_com/orders/show.blade.php
@@ -129,7 +129,7 @@
 							</tr>
 						</table>
 
-            @if($order->price!==0)
+            @if($order->price!==0.00)
 						<a type="button" id="bottonsubmit" style="height:33px; margin-left:20px; margin-left:320px;" href="javascript:;"
 						 class="btn btn-primary btn-sm sbtn">提交</a>
 						<a type="button" id="btn-wechat" style="height:33px; margin-left:20px; margin-left:320px;display: none" href="javascript:;"
