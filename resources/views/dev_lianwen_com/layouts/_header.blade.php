02c522ecc9028aba517b0db356f490b1a1a44d59
阿沙冲冲冲 <361719196@qq.com>
2020-05-26 15:33:50 +0800

    test

--- resources/views/dev_lianwen_com/layouts/_header.blade.php
+++ resources/views/dev_lianwen_com/layouts/_header.blade.php
@@ -63,8 +63,9 @@
     @endauth
   </div>
 </nav>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
 <script type="text/javascript" src="{{ asset('asset/js/jquery.min.js') }}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 <script>
    toastr.options = {
 
