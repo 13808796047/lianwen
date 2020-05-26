c86b1a88ce11af5fec07cefb42fc930afd5a9c69
阿沙冲冲冲 <361719196@qq.com>
2020-05-26 15:30:03 +0800

    test

--- resources/views/dev_lianwen_com/layouts/_header.blade.php
+++ resources/views/dev_lianwen_com/layouts/_header.blade.php
@@ -65,14 +65,37 @@
 </nav>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 <script>
+   toastr.options = {
 
+     "closeButton": true, //是否显示关闭按钮
+
+     "debug": true, //是否使用debug模式
+
+     "showDuration": "1000",//显示的动画时间
+
+     "hideDuration": "1000",//消失的动画时间
+
+     "positionClass": "toast-center-center",//弹出窗的位置
+
+     "timeOut": "1000", //展现时间
+
+     "extendedTimeOut": "1000",//加长展示时间
+
+     "showEasing": "swing",//显示时的动画缓冲方式
+
+     "hideEasing": "linear",//消失时的动画缓冲方式
+
+     "showMethod": "fadeIn",//显示时的动画方式
+
+     "hideMethod": "fadeOut" //消失时的动画方式
+   };
 
   @unless(Auth::user())
     let atag = document.querySelector('#categories').getElementsByTagName("a");
     for(let i in atag){
       atag[i].onclick = function(event){
           event.preventDefault()
-
+          toastr.error('当前未登录账号，请登录后再操作');
       }
     }
   @endunless
