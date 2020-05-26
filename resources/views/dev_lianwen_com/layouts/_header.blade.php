53c159a22ced9f072c799b67fe83bacea922ad2b
阿沙冲冲冲 <361719196@qq.com>
2020-05-26 15:50:48 +0800

    test

--- resources/views/dev_lianwen_com/layouts/_header.blade.php
+++ resources/views/dev_lianwen_com/layouts/_header.blade.php
@@ -63,8 +63,8 @@
     @else
     <div class="flex justify-content-around w-25 align-items-center">
 
-        <span  class="logout inline-block text-sm px-4 py-1 bg-teal-500 border rounded text-white border-white hover:border-transparent hover:text-black-500 hover:bg-red
-          lg:mt-0">旧版入口</span>
+        <a href="https://2019.lianwen.com" class="inline-block text-sm px-4 py-1 bg-teal-500 border rounded text-white border-white hover:border-transparent hover:text-black-500 hover:bg-red
+          lg:mt-0">旧版入口</a>
     </div>
     @endauth
 
