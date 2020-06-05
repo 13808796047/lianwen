7ca917da4896c6841cb8da0f08c40304feda321f
阿沙冲冲冲 <361719196@qq.com>
2020-06-05 16:46:07 +0800

    更新线上

--- resources/views/www_lianwen_com/pages/index.blade.php
+++ resources/views/www_lianwen_com/pages/index.blade.php
@@ -172,6 +172,7 @@
   </div>
 @stop
 @section('scripts')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
   <script>
     $(function () {
       {{--      @auth--}}
@@ -288,6 +289,51 @@
           .siblings(".listbox")
           .css("display", "none");
       });
+      toastr.options = {
+
+"closeButton": true, //是否显示关闭按钮
+
+"debug": true, //是否使用debug模式
+
+"showDuration": "1000",//显示的动画时间
+
+"hideDuration": "1000",//消失的动画时间
+
+"positionClass": "toast-cent-cent",//弹出窗的位置
+
+"timeOut": "1000", //展现时间
+
+"extendedTimeOut": "1000",//加长展示时间
+
+"showEasing": "swing",//显示时的动画缓冲方式
+
+"hideEasing": "linear",//消失时的动画缓冲方式
+
+"showMethod": "fadeIn",//显示时的动画方式
+
+"hideMethod": "fadeOut" //消失时的动画方式
+};
+
+@unless(Auth::user())
+console.log(window.location.href)
+if(window.location.href=='https://www.lianwen.com/'){
+ $('#jbrk').css('display','block')
+}else{
+ $('#jbrk').css('display','none')
+}
+if(window.location.href=='https://www.lianwen.com/ai_rewrite'){
+ $('#dlzcrk').css('display','block')
+}else{
+ $('#dlzcrk').css('display','none')
+}
+let atag = document.getElementsByClassName("istoaster");
+console.log(atag,3131)
+for(let i in atag){
+ atag[i].onclick = function(event){
+     event.preventDefault()
+     toastr.error('当前未登录账号，请登录后再操作');
+ }
+}
     });
   </script>
 @stop
