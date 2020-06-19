512de77568fd36f5bb998d8f287a493e2c7b9677
阿沙冲冲冲 <361719196@qq.com>
2020-06-19 16:31:11 +0800

    验证码登录

--- resources/views/weipu_lianwen_com/pages/index.blade.php
+++ resources/views/weipu_lianwen_com/pages/index.blade.php
@@ -706,7 +706,6 @@
           verification_key: verification_key,
           type: 'phone'
         }).then(res => {
-          console.log(res,"系啊范德萨发生")
           swal("提示",'登录成功', "success");
           location.reload();
         }).catch(err => {
