68aa5d6509878b5010a916c1278911fbd11634f2
Summer <csy13808796047@gmail.com>
2020-06-19 15:55:13 +0800

    注册增加查重次数:seedling:

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -18,12 +18,27 @@ class VerificationCodesController extends Controller
             //生成4位随机数,左侧补0
             $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
         }
+        $data = [
+            'code' => $code,
+        ];
+        $domain = request()->getHost();
+        switch ($domain) {
+            case config('app.host.wf_host'):
+                $data['product'] = '万方查重';
+                break;
+            case config('app.host.wp_host'):
+                $data['product'] = '维普查重';
+                break;
+            case config('app.host.pp_host'):
+                $data['product'] = 'paperPass检测';
+                break;
+            default:
+                $data['product'] = '学信检测';
+        }
         try {
             $result = $easySms->send($phone, [
                 'template' => config('easysms.gateways.aliyun.templates.register'),
-                'data' => [
-                    'code' => $code,
-                ]
+                'data' => $data
             ]);
         } catch (\Overtrue\EasySms\Exceptions\NoGatewayAvailableException $exception) {
             $message = $exception->getException('aliyun')->getMessage();
