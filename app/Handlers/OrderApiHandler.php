02c811516b02cc1dd5650c7e7a98978d8c1d3117
Summer <csy13808796047@gmail.com>
2020-06-22 11:47:38 +0800

    发表时间为空

--- app/Handlers/OrderApiHandler.php
+++ app/Handlers/OrderApiHandler.php
@@ -77,11 +77,11 @@ class OrderApiHandler
         switch ($order->category->cid) {
             case 8:
 //                $body = Arr::add($data, 'endDate', $order->endDate);
-                $data['postDate'] = $order->endDate . ' 00:00:00' ?? '';
+                $data['postDate'] = $order->endDate . ' 00:00:00' ?: '';
                 break;
             case 23:
 //                $body = Arr::add($data, 'publishdate', $order->publishdate);
-                $data['postDate'] = $order->publishdate . ' 00:00:00' ?? '';
+                $data['postDate'] = $order->publishdate . ' 00:00:00' ?: '';
                 break;
         }
         // 构建请求参数
