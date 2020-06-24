03606e21a55672294a81dd78bf660b5c3cf21cc9
Summer <csy13808796047@gmail.com>
2020-06-24 14:48:15 +0800

    unionid

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -91,7 +91,8 @@ class AuthorizationsController extends Controller
             throw new AuthenticationException('参数code错误，未获取用户信息');
         }
         $data = $app->auth->session($code);
-        if($iv = $request->input('iv') && $encryptData = $request->input('encryptData')) {
+        if($iv = $request->iv) {
+            $encryptData = $request->encryptData;
             info($iv);
             info($encryptData);
             $decryptedData = $app->encryptor->decryptData($data['session_key'], $iv, $encryptData);
