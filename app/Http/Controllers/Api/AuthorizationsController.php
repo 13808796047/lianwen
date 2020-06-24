837e2a52fb7db79238f01266ca5fe5f4dfcd7f0c
Summer <csy13808796047@gmail.com>
2020-06-24 13:32:51 +0800

    发表时间为空

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -92,6 +92,7 @@ class AuthorizationsController extends Controller
             throw new AuthenticationException('参数code错误，未获取用户信息');
         }
         $data = $app->auth->session($code);
+        info($data);
         // 如果结果错误，说明 code 已过期或不正确，返回 401 错误
         if(isset($data['errcode'])) {
             throw new AuthenticationException('code 不正确');
