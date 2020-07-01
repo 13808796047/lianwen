5b09b44023d90907f2c4c7f0244ea4f0998e8008
Summer <csy13808796047@gmail.com>
2020-07-01 10:43:48 +0800

    小程序绑定手机号

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -38,47 +38,18 @@ class UserService
         $mini_program_user = auth()->user();
         $phone_user = User::where('phone', $phone)->first();
         $mini_program_user = DB::transaction(function() use ($request, $mini_program_user, $phone_user, $phone) {
-//            if(!$phone_user) {
-//                $mini_program_user->update([
-//                    'phone' => $phone,
-//                ]);
-//            }
             if($phone_user) {
-                $phone_user->delete();
                 foreach($phone_user->orders as $order) {
                     $order->update([
                         'userid' => $mini_program_user->id,
                     ]);
                 }
+                $phone_user->delete();
             }
             $mini_program_user->update([
                 'phone' => $phone,
                 'password' => $phone_user->password ?? "",
             ]);
-//            $data = [
-//                'weixin_unionid' => $phone_user->weixin_unionid,
-//            ];
-//
-//            if($phone_user->weixin_unionid) {
-//                switch (request()->getHost()) {
-//                    case config('app.host.dev_host'):
-//                        $data['dev_weapp_openid'] = $phone_user->dev_weapp_openid;
-//                        break;
-//                    case config('app.host.wf_host'):
-//                        $data['wf_weapp_openid'] = $phone_user->wf_weapp_openid;
-//                        break;
-//                    case config('app.host.wp_host'):
-//                        $data['wp_weapp_openid'] = $phone_user->wp_weapp_openid;
-//                        break;
-//                    case config('app.host.pp_host'):
-//                        $data['pp_weapp_openid'] = $phone_user->pp_weapp_openid;
-//                        break;
-//                    default:
-//                        $data['cn_weapp_openid'] = $phone_user->cn_weapp_openid;
-//                }
-//                $mini_program_user->update($data);
-//            }
-
             return $mini_program_user;
         });
         return $mini_program_user;
