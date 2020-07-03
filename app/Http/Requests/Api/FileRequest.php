6417344fecf22f025644d3f1ed7f9dc73aadbc89
Summer <csy13808796047@gmail.com>
2020-07-03 16:26:42 +0800

    后台查询

--- app/Http/Requests/Api/FileRequest.php
+++ app/Http/Requests/Api/FileRequest.php
@@ -8,7 +8,7 @@ class FileRequest extends FormRequest
     public function rules()
     {
         return [
-            'file' => 'required'
+            'file' => 'required|file|mimes:docx,txt'
         ];
     }
 
