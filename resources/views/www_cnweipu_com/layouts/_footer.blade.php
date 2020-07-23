486ba46b6969c135f82a22b2e26feefd39f8dab4
阿沙冲冲冲 <361719196@qq.com>
2020-07-23 11:27:12 +0800

    页脚更改

--- resources/views/www_cnweipu_com/layouts/_footer.blade.php
+++ resources/views/www_cnweipu_com/layouts/_footer.blade.php
@@ -1,5 +1,5 @@
 <div id="lwfooter" class="w-full flex  justify-center bg-gray-800 absolute" style="height: 60px;bottom: 0;">
   <div class="p-3 text-white">
-    COPYRIGHT © 2005-2020 深圳智汇教育有限公司 粤ICP备16093775号 维普检测 版权所有 客服热线：400-823-8869
+    COPYRIGHT © 2005-2021 CNWEIPU.COM 维普查重 版权所有 客服热线：400-823-8869
   </div>
 </div>
