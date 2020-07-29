d2a498e4f9a66608780a24f82c25aae17f1e8c43
阿沙冲冲冲 <361719196@qq.com>
2020-07-29 16:32:22 +0800

    bug

--- resources/views/wanfang_lianwen_com/orders/show.blade.php
+++ resources/views/wanfang_lianwen_com/orders/show.blade.php
@@ -1,9 +1,10 @@
 @extends('domained::layouts.app')
 @section('title', '查看订单')
 @section('styles')
+
+  <link href="{{asset('asset/css/theme.css')}}" rel="stylesheet"/>
   <link href="{{asset('asset/css/check.css')}}" rel="stylesheet"/>
-  <!-- <link href="{{asset('asset/css/theme.css')}}" rel="stylesheet"/>
-  <link href="{{asset('asset/css/font-awesome.min.css')}}" rel="stylesheet"/>
+  <!-- <link href="{{asset('asset/css/font-awesome.min.css')}}" rel="stylesheet"/>
   <link href="{{asset('asset/css/alertify.css')}}" rel="stylesheet"/> -->
   <style>
     .curfont{
